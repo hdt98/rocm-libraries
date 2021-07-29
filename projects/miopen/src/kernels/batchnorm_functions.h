hdt98/rocm-libraries

commit 11610f09fd3eed4bff05fa085897005e938b3977

[BN][Navi21] Get rid of "#undef __AMDGCN__" (#1037)
[ROCm/MIOpen commit: bfb1798954e1e0ac29994f7fdc00aea17eeaa502]

projects/miopen/src/kernels/batchnorm_functions.h
@@ -129,10 +129,15 @@
 #define MIO_BN_MAXN 65
 #endif
 
+#ifdef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 1
+#endif
+
 #ifndef MIO_BN_NODPP
 #define MIO_BN_NODPP 0
 #elif(MIO_BN_NODPP == 1 && MIO_BN_VARIANT != 0)
-#undef __AMDGCN__
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 0
 #endif
 
 #ifndef MIO_SAVE_MEAN_VARIANCE
@@ -163,8 +168,9 @@ static inline void running_stash(global _FLOAT_PREC* resultRunningMean,
     resultRunningMean[channel] =
         (_FLOAT_PREC)mad(mean, (_FLOAT_ACCUM)expAvgFactor, pvt_newRunMean); // newMean*factor + tmp
     const _FLOAT_ACCUM adjust = (_FLOAT_ACCUM)(
-        (MIO_BN_NHW == 1) ? variance : variance * ((_FLOAT_ACCUM)MIO_BN_NHW /
-                                                   ((_FLOAT_ACCUM)MIO_BN_NHW - (_FLOAT_ACCUM)1.0)));
+        (MIO_BN_NHW == 1) ? variance
+                          : variance * ((_FLOAT_ACCUM)MIO_BN_NHW /
+                                        ((_FLOAT_ACCUM)MIO_BN_NHW - (_FLOAT_ACCUM)1.0)));
     resultRunningVariance[channel] = (_FLOAT_PREC)(
         (1 - (_FLOAT_ACCUM)expAvgFactor) * (_FLOAT_ACCUM)(*(resultRunningVariance + channel)) +
         (_FLOAT_ACCUM)expAvgFactor * adjust);

projects/miopen/src/kernels/MIOpenBatchNormActivBwdPerAct.cl
@@ -33,8 +33,10 @@
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
 #endif
 
-#if(MIO_BN_GFX1030 == 1 && __AMDGCN__)
-#undef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 0
+#if defined(__AMDGCN__) && MIO_BN_GFX1030 != 1
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 1
 #endif
 
 #include "batchnorm_functions.h"

projects/miopen/src/kernels/MIOpenBatchNormActivBwdSpatial.cl
@@ -31,8 +31,10 @@
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
 #endif
 
-#if(MIO_BN_GFX1030 == 1 && __AMDGCN__)
-#undef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 0
+#if defined(__AMDGCN__) && MIO_BN_GFX1030 != 1
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 1
 #endif
 
 #include "batchnorm_functions.h"
@@ -167,7 +169,7 @@ MIOpenBatchNormActivBwdSpatial(const __global _FLOAT* __restrict x_in,
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);
@@ -416,7 +418,7 @@ MIOpenBatchNormActivBwdSpatial(const __global _FLOAT* __restrict x_in,
 #endif
     barrier(CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);
@@ -610,7 +612,7 @@ MIOpenBatchNormActivBwdSpatial(const __global _FLOAT* __restrict x_in,
 
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);

projects/miopen/src/kernels/MIOpenBatchNormActivFwdTrainSpatial.cl
@@ -32,8 +32,10 @@
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
 #endif
 
-#if(MIO_BN_GFX1030 == 1 && __AMDGCN__)
-#undef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 0
+#if defined(__AMDGCN__) && MIO_BN_GFX1030 != 1
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 1
 #endif
 
 #include "batchnorm_functions.h"
@@ -127,7 +129,7 @@ MIOpenBatchNormActivFwdTrainSpatial(float INHW,
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -325,7 +327,7 @@ MIOpenBatchNormActivFwdTrainSpatial(
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
 // REDUCE MEAN AND VARIANCE -----------------------
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -503,7 +505,7 @@ MIOpenBatchNormActivFwdTrainSpatial(
     }
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
 #if MIOPEN_USE_FP16 == 1
     local float lcl_data[MIO_BN_LDS_SIZE];
     lcl_data[lid] = (float)mean;

projects/miopen/src/kernels/MIOpenBatchNormBwdSpatial.cl
@@ -32,8 +32,10 @@
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
 #endif
 
-#if(MIO_BN_GFX1030 == 1 && __AMDGCN__)
-#undef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 0
+#if defined(__AMDGCN__) && MIO_BN_GFX1030 != 1
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 1
 #endif
 
 #include "batchnorm_functions.h"
@@ -127,7 +129,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
         variance = mad(batchvalues[MIO_BN_NLOOPM], batchvalues[MIO_BN_NLOOPM], variance);
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -184,7 +186,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);
@@ -369,7 +371,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
 
     barrier(CLK_LOCAL_MEM_FENCE);
 // REDUCE MEAN AND VARIANCE -----------------------
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -450,7 +452,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
 #endif
     barrier(CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);
@@ -595,7 +597,7 @@ MIOpenBatchNormBwdSpatialFinalMeanVariance(__global _FLOAT* __restrict meanvarbu
         }
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -650,7 +652,7 @@ MIOpenBatchNormBwdSpatialMeanVariance(const __global _FLOAT* __restrict in,
         }
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_NGRPS];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_NGRPS];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)1.0, lcl_data_x, lcl_data_y, ylid);
@@ -729,7 +731,7 @@ MIOpenBatchNormBwdSpatialDScaleDBias(const __global _FLOAT* x_in,
     }
 
 // REDUCE over DS and DB
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&dscale, &dbias, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, ylid);
@@ -777,7 +779,7 @@ MIOpenBatchNormBwdSpatialFinalDScaleDBias(__global _FLOAT* buff,
         }
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_NGRPS];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_NGRPS];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);
@@ -953,7 +955,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
     }
 
 // REDUCE MEAN AND VARIANCE -----------------------
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -1004,7 +1006,7 @@ MIOpenBatchNormBwdSpatial(const __global _FLOAT* __restrict x_in,
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x2[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y2[MIO_BN_LDS_SIZE];
     lds_reduce2(&ds, &db, (_FLOAT_ACCUM)1.0, lcl_data_x2, lcl_data_y2, lid);

projects/miopen/src/kernels/MIOpenBatchNormFwdTrainSpatial.cl
@@ -32,8 +32,10 @@
 #pragma clang diagnostic ignored "-Wsometimes-uninitialized"
 #endif
 
-#if(MIO_BN_GFX1030 == 1 && __AMDGCN__)
-#undef __AMDGCN__
+#define MIOPEN_USE_AMDGCN 0
+#if defined(__AMDGCN__) && MIO_BN_GFX1030 != 1
+#undef MIOPEN_USE_AMDGCN
+#define MIOPEN_USE_AMDGCN 1
 #endif
 
 #include "batchnorm_functions.h"
@@ -119,7 +121,7 @@ MIOpenBatchNormFwdTrainSpatial(const __global _FLOAT* __restrict in,
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -306,7 +308,7 @@ MIOpenBatchNormFwdTrainSpatial(const __global _FLOAT* __restrict in,
 #endif
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -503,7 +505,7 @@ MIOpenBatchNormFwdTrainSpatialFinalMeanVariance(
         }
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -574,7 +576,7 @@ MIOpenBatchNormFwdTrainSpatialMeanVariance(const __global _FLOAT* __restrict in,
         }
     }
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)1.0, lcl_data_x, lcl_data_y, ylid);
@@ -657,7 +659,7 @@ MIOpenBatchNormFwdTrainSpatial(const __global _FLOAT* __restrict in,
     }
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);
@@ -776,7 +778,7 @@ __kernel void MIOpenBatchNormFwdTrainSpatial(const __global _FLOAT* __restrict i
     }
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
     local _FLOAT_ACCUM lcl_data_x[MIO_BN_LDS_SIZE];
     local _FLOAT_ACCUM lcl_data_y[MIO_BN_LDS_SIZE];
     lds_reduce2(&mean, &variance, (_FLOAT_ACCUM)INHW, lcl_data_x, lcl_data_y, lid);

commit 01ec3a072eaa6510f4dfdb456665c13223cae2f4

[Navi21][HOTFIX] Fix kernel build issues introduced in PR #1037 (#1065)
[ROCm/MIOpen commit: 9cb5820eaccdad9f96ea3677b29918493f79696d]

projects/miopen/src/kernels/batchnorm_functions.h
@@ -129,9 +129,16 @@
 #define MIO_BN_MAXN 65
 #endif
 
-#ifdef __AMDGCN__
+// TODO: Spaghetti code!!!
+// MIOPEN_USE_AMDGCN may be defined before this header.
+#ifndef MIOPEN_USE_AMDGCN
+#if defined(__AMDGCN__) && !(defined(MIO_BN_GFX1030) && MIO_BN_GFX1030 == 1)
 #define MIOPEN_USE_AMDGCN 1
+#else
+#define MIOPEN_USE_AMDGCN 0
 #endif
+#endif
+// MIOPEN_USE_AMDGCN is guaranteed to be defined at this point.
 
 #ifndef MIO_BN_NODPP
 #define MIO_BN_NODPP 0

projects/miopen/src/kernels/reduction_functions.h
@@ -1,5 +1,5 @@
 
-#ifndef __AMDGCN__
+#if !MIOPEN_USE_AMDGCN
 static inline void lds_reduce2(_FLOAT_ACCUM* x,
                                _FLOAT_ACCUM* y,
                                _FLOAT_ACCUM scale,
@@ -57,7 +57,7 @@ regLDSreduce(_FLOAT_ACCUM* value, local _FLOAT_ACCUM* data, uint localID, _FLOAT
 
 #endif
 
-#ifdef __AMDGCN__
+#if MIOPEN_USE_AMDGCN
 static inline void dpp_reduction(_FLOAT_ACCUM* temp_sum)
 {
     __asm__ volatile("s_nop 4\n"