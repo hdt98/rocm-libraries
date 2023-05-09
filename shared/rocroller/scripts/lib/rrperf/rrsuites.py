ae67efc05d0c23fd0c6563b9294dc4a6d6efcee2
Nemanich, Brad <Brad.Nemanich@amd.com>
2023-05-09 07:58:46 -0500

    Add some new prefetch tests
    * Add some new prefetch tests
    
    [ROCm/rocRoller commit: f19b246d3e4c0d70ac5221289f40bb1e1eaaeb72]

--- shared/rocroller/scripts/lib/rrperf/rrsuites.py
+++ shared/rocroller/scripts/lib/rrperf/rrsuites.py
@@ -121,6 +121,19 @@ def hgemm():
         mac_m=128,
         mac_n=256,
         mac_k=16,
+        workgroup_size_x=128,
+        workgroup_size_y=2,
+        prefetchInFlight=2,
+        prefetchLDSFactor=2,
+        **fp16,
+    )
+    yield GEMMRun(
+        M=7680,
+        N=8448,
+        K=8192,
+        mac_m=128,
+        mac_n=256,
+        mac_k=16,
         workgroup_size_x=64,
         workgroup_size_y=4,
         **fp16,
@@ -153,6 +166,18 @@ def hgemm():
         mac_k=16,
         workgroup_size_x=64,
         workgroup_size_y=4,
+        **fp16,
+    )
+
+    yield GEMMRun(
+        **tensile_guidepost_HGEMM,
+        mac_m=128,
+        mac_n=256,
+        mac_k=16,
+        workgroup_size_x=128,
+        workgroup_size_y=2,
+        prefetchInFlight=2,
+        prefetchLDSFactor=2,
         **fp16,
     )
 
