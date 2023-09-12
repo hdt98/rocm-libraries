329137a49ae3fc7686e145c918d59d7bb83a7490
Nelson, Bryant <Bryant.Nelson@amd.com>
2023-09-12 08:26:28 -0500

    Splitup and Organize GEMM Client
    [ROCm/rocRoller commit: 0abcb64669162026263ca02bc36e29c9cf502870]

--- shared/rocroller/client/GEMMParameters.cpp
+++ shared/rocroller/client/GEMMParameters.cpp
@@ -2,55 +2,59 @@
 
 namespace rocRoller
 {
-    namespace GEMMClient
+    namespace Client
     {
-
-        std::string SolutionParameters::generateKernelName() const
+        namespace GEMMClient
         {
-            std::ostringstream rv;
-            rv << "GEMM_" << toString(problemParams.transA) << toString(problemParams.transB);
 
-            rv << "_";
-            for(auto const& t : {problemParams.typeA,
-                                 problemParams.typeB,
-                                 problemParams.typeC,
-                                 problemParams.typeD,
-                                 problemParams.typeAcc})
-                rv << t.substr(0, 1);
-
-            rv << "_MT";
-            rocRoller::streamJoin(rv, std::vector{macM, macN, macK}, "x");
-
-            rv << "_WG";
-            rocRoller::streamJoin(rv, std::vector{workgroupSizeX, workgroupSizeY}, "x");
-
-            rv << "_LDS";
-            rocRoller::streamJoin(rv, std::vector{loadLDSA, loadLDSB, storeLDSD}, "");
-
-            rv << "_UNROLL";
-            rocRoller::streamJoin(rv, std::vector{unrollX, unrollY}, "x");
-
-            if(prefetch)
+            std::string SolutionParameters::generateKernelName() const
             {
-                rv << "_PF";
-                rocRoller::streamJoin(rv, std::vector{prefetchInFlight, prefetchLDSFactor}, "x");
+                std::ostringstream rv;
+                rv << "GEMM_" << toString(problemParams.transA) << toString(problemParams.transB);
+
+                rv << "_";
+                for(auto const& t : {problemParams.typeA,
+                                     problemParams.typeB,
+                                     problemParams.typeC,
+                                     problemParams.typeD,
+                                     problemParams.typeAcc})
+                    rv << t.substr(0, 1);
+
+                rv << "_MT";
+                rocRoller::streamJoin(rv, std::vector{macM, macN, macK}, "x");
+
+                rv << "_WG";
+                rocRoller::streamJoin(rv, std::vector{workgroupSizeX, workgroupSizeY}, "x");
+
+                rv << "_LDS";
+                rocRoller::streamJoin(rv, std::vector{loadLDSA, loadLDSB, storeLDSD}, "");
+
+                rv << "_UNROLL";
+                rocRoller::streamJoin(rv, std::vector{unrollX, unrollY}, "x");
+
+                if(prefetch)
+                {
+                    rv << "_PF";
+                    rocRoller::streamJoin(
+                        rv, std::vector{prefetchInFlight, prefetchLDSFactor}, "x");
+                }
+
+                rv << "_" << scheduler;
+
+                return rv.str();
             }
 
-            rv << "_" << scheduler;
-
-            return rv.str();
-        }
-
-        std::string toString(TransposeType trans)
-        {
-            switch(trans)
+            std::string toString(TransposeType trans)
             {
-            case TransposeType::T:
-                return "T";
-            case TransposeType::N:
-                return "N";
-            default:
-                rocRoller::Throw<rocRoller::FatalError>("Unknown transpose type");
+                switch(trans)
+                {
+                case TransposeType::T:
+                    return "T";
+                case TransposeType::N:
+                    return "N";
+                default:
+                    rocRoller::Throw<rocRoller::FatalError>("Unknown transpose type");
+                }
             }
         }
     }
--- shared/rocroller/client/include/GEMMParameters.hpp
+++ shared/rocroller/client/include/GEMMParameters.hpp
@@ -4,29 +4,32 @@
 #include <vector>
 
 #include <rocRoller/DataTypes/DataTypes.hpp>
-#include <rocRoller/Utilities/Error.hpp>
 #include <rocRoller/Utilities/Utils.hpp>
+
+#include "BenchmarkSolution.hpp"
 
 namespace rocRoller
 {
-    namespace GEMMClient
+    namespace Client
     {
+        namespace GEMMClient
+        {
 
-        /**
+            /**
          * @brief Indicates whether a matrix is supplied in transposed form or not
          *
          */
-        enum class TransposeType
-        {
-            T,
-            N,
+            enum class TransposeType
+            {
+                T,
+                N,
 
-            Count
-        };
+                Count
+            };
 
-        std::string toString(TransposeType trans);
+            std::string toString(TransposeType trans);
 
-        /**
+            /**
          * @brief Parameters of a GEMM problem.
          *  D = alpha * A * B + beta * C
          * where
@@ -36,80 +39,66 @@ namespace rocRoller
          * alpha and beta are scalars
          *
          */
-        struct ProblemParameters
-        {
-            int   m;
-            int   n;
-            int   k;
-            float alpha;
-            float beta;
+            struct ProblemParameters
+            {
+                int   m;
+                int   n;
+                int   k;
+                float alpha;
+                float beta;
 
-            // Datatype of inputs and outputs
-            std::string typeA;
-            std::string typeB;
-            std::string typeC;
-            std::string typeD;
-            std::string typeAcc;
+                // Datatype of inputs and outputs
+                std::string typeA;
+                std::string typeB;
+                std::string typeC;
+                std::string typeD;
+                std::string typeAcc;
 
-            TransposeType transA;
-            TransposeType transB;
-        };
+                TransposeType transA;
+                TransposeType transB;
+            };
 
-        /**
+            /**
          * @brief Solution parameters common to all approaches to solving GEMMs.
          *
          */
-        struct SolutionParameters
-        {
-            ProblemParameters problemParams;
+            struct SolutionParameters
+            {
+                ProblemParameters problemParams;
 
-            // Macro Tile Size
-            int macM;
-            int macN;
-            int macK;
+                // Macro Tile Size
+                int macM;
+                int macN;
+                int macK;
 
-            // Number of wave tiles to execute per workgroup
-            int workgroupSizeX = 64;
-            int workgroupSizeY = 1;
+                // Number of wave tiles to execute per workgroup
+                int workgroupSizeX = 64;
+                int workgroupSizeY = 1;
 
-            bool loadLDSA  = true;
-            bool loadLDSB  = true;
-            bool storeLDSD = true;
+                bool loadLDSA  = true;
+                bool loadLDSB  = true;
+                bool storeLDSD = true;
 
-            bool prefetch          = false;
-            int  prefetchInFlight  = 2;
-            int  prefetchLDSFactor = 0;
-            bool betaInFma         = true;
+                bool prefetch          = false;
+                int  prefetchInFlight  = 2;
+                int  prefetchLDSFactor = 0;
+                bool betaInFma         = true;
 
-            // Unroll Options
-            unsigned int unrollX = 0;
-            unsigned int unrollY = 0;
+                // Unroll Options
+                unsigned int unrollX = 0;
+                unsigned int unrollY = 0;
 
-            std::string scheduler;
-            bool        matchMemoryAccess;
+                std::string scheduler;
+                bool        matchMemoryAccess;
 
-            std::string generateKernelName() const;
-        };
+                std::string generateKernelName() const;
+            };
 
-        struct RunParameters
-        {
-            int device;
-
-            int numWarmUp;
-            int numOuter;
-            int numInner;
-        };
-
-        struct Result
-        {
-            SolutionParameters solutionParams;
-            RunParameters      runParams;
-
-            size_t              kernelGenerate;
-            size_t              kernelAssemble;
-            std::vector<size_t> kernelExecute;
-            bool                checked = false;
-            bool                correct = true;
-        };
+            struct Result
+            {
+                SolutionParameters                  solutionParams;
+                rocRoller::Client::BenchmarkResults benchmarkResults;
+            };
+        }
     }
 }
