45230f1f48a9adfffadea1076caab4efd01cd103
Matthew Emmett <matthew.emmett@amd.com>
2025-02-25 15:19:32 -0700

    Remove Graupel.
    [ROCm/rocRoller commit: ef51f0619f45cf184c4768371e8c342253bacd67]

--- shared/rocroller/utils/graupel/src/model/instanceManger.d.ts
+++
@@ -1,9 +0,0 @@
-import { Core, Stylesheet } from "cytoscape";
-
-type Instance = {
-  cy: Core;
-  id: String;
-  removeElements: Function;
-  restoreElements: Function;
-  style: Promise<Stylesheet>;
-};
