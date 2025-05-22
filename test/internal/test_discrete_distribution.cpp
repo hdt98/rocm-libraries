935b5d6a12120299bc359a3e719eab0e60ca0163
NguyenNhuDi <dinguyennhu@gmail.com>
2025-05-22 17:14:30 -0600

    re structured discrete distribution tests

--- test/internal/test_discrete_distribution.cpp
+++ test/internal/test_discrete_distribution.cpp
@@ -26,300 +26,156 @@
 #include <vector>
 #include <rocrand/rocrand_discrete.h>
 
-#define HIP_CHECK(state) ASSERT_EQ(state, hipSuccess)
-#define TEST_SIZE 1000000
+#define HIP_CHECK(cmd)                                                                          \
+    do                                                                                          \
+    {                                                                                           \
+        auto error = (cmd);                                                                     \
+        if(error != hipSuccess)                                                                 \
+        {                                                                                       \
+            std::cerr << "Encountered HIP error (" << hipGetErrorString(error) << ") at line "  \
+                      << __LINE__ << " in file " << __FILE__ << "\n";                           \
+            exit(-1);                                                                           \
+        }                                                                                       \
+    } while(0)                                                                                  \
 
-template <typename T>
-__global__ void discrete_alias_kernel(unsigned int * output, T * input, rocrand_discrete_distribution_st &dis,  const size_t N){
-    auto bIdx = blockIdx.x, tIdx = threadIdx.x, bSize = blockDim.x;
-    auto idx = bIdx * bSize + tIdx;
-
-    if(idx >= N) 
-        return;
-    output[idx] = rocrand_device::detail::discrete_alias(input[idx], dis);
-}
-
-template <typename T>
-__global__ void discrete_cdf_kernel(unsigned int * output, T * input, rocrand_discrete_distribution_st &dis,  const size_t N){
-    auto bIdx = blockIdx.x, tIdx = threadIdx.x, bSize = blockDim.x;
-    auto idx = bIdx * bSize + tIdx;
-
-    if(idx >= N) 
-        return;
-    output[idx] = rocrand_device::detail::discrete_cdf(input[idx], dis);
-}
-
-template <typename T>
-struct run_discrete_distribution_tests{
-    std::vector<std::vector<double>> allProbabilities;
-    std::vector<size_t> sizes;
-    size_t totalSize;
+#define ROCRAND_CHECK(cmd)                                                                      \
+    do                                                                                          \
+    {                                                                                           \
+        auto status = cmd;                                                                      \
+        if(status != 0){                                                                        \
+            std::cerr << "Encountered ROCRAND error: " << status << "at line"                   \
+            << __LINE__ << " in file " << __FILE__ << "\n";                                     \
+            exit(-1);                                                                           \
+        }                                                                                       \
+    } while(0)                                                                                  \
 
 
-    run_discrete_distribution_tests(){
-        // From top to bottom:
-        /**
-         * discrete uniform
-         * discrete normal (6 sided dice roll)
-         * discrete poisson (lambda = 10)
-         * discrete log normal (mean = 1, std = 5)
-         */
-        std::vector<std::vector<T>> aProb = {
-            {10, 10, 10, 10},
-            {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1},
-            {1234, 1677, 1519, 1032, 561, 254, 98, 33, 10, 2},
-            {1, 2, 8, 4, 3, 2, 1}
-        };
 
-        this->totalSize = aProb.size();
+using DiscreteDataType = ::testing::Types<double, unsigned int, unsigned long, unsigned long long int>;
 
-        this->allProbabilities.assign(this->totalSize, std::vector<double>());
-        this->sizes.assign(this->totalSize, 0);
-
-        for(size_t i = 0; i < this->totalSize; i++){
-            this->sizes[i] = aProb[i].size();
-            this->allProbabilities[i].assign(this->sizes[i], 0);
-            
-            T sum = 0;
-
-            for(const T & x : aProb[i])
-                sum += x;
-            
-            double dSum = static_cast<double>(sum);
-            for(size_t ii = 0; ii < this->sizes[i]; ii++)
-                this->allProbabilities[i][ii] = static_cast<double>(aProb[i][ii]) / dSum; 
-        }      
-    }
-    
-    template<typename KernelCallFunc>
-    void runTestInternal(const KernelCallFunc & kcf, const size_t inputSize){
-        T * hInput = new T[inputSize];
-        T * dInput;
-
-        unsigned int * hOutput = new unsigned int [inputSize];
-        unsigned int * dOutput;
-        
-        HIP_CHECK(hipMalloc(&dInput, sizeof(T) * inputSize));
-        HIP_CHECK(hipMalloc(&dOutput, sizeof(unsigned int) * inputSize));
-
-        std::random_device                          rd;
-        std::mt19937                                gen(rd());
-        
-        // If unsigned long is passed into the uniform distribution, it will cause
-        // memory access faults
-        if(std::is_same<T, unsigned long>::value){
-            unsigned int maxi = std::numeric_limits<unsigned int>::max();
-            unsigned int mini = std::numeric_limits<unsigned int>::min();
-            std::uniform_int_distribution<unsigned int> dis(mini, maxi);
-
-            for(size_t i = 0; i < inputSize; i++)
-                hInput[i] = dis(gen);
-        }
-        else{
-            T maxi = std::numeric_limits<T>::max();
-            T mini = std::numeric_limits<T>::min();
-            std::uniform_int_distribution<T> dis(mini, maxi);
-
-            for(size_t i = 0; i < inputSize; i++)
-                hInput[i] = dis(gen);
-        }
-        
-        HIP_CHECK(hipMemcpy(dInput, hInput, sizeof(T) * inputSize, hipMemcpyHostToDevice));
-
-        for(size_t i = 0; i < totalSize; i++){
-            std::vector<double> & prob = allProbabilities[i];
-            size_t N = sizes[i];
-
-            rocrand_discrete_distribution discrete_distribution;
-            HIP_CHECK(rocrand_create_discrete_distribution(prob.data(), prob.size(), 0, &discrete_distribution));
-
-            kcf(dOutput, dInput, *discrete_distribution, inputSize);
-
-            HIP_CHECK(hipMemcpy(hOutput, dOutput, sizeof(unsigned int) * inputSize, hipMemcpyDeviceToHost));
-
-            std::vector<double> count(N, 0);
-            for(size_t j = 0; j < inputSize; j++)
-                count[hOutput[j]]++;
-
-            for(size_t j = 0; j < N; j++){
-                double res = count[j] / static_cast<double>(inputSize);
-                double eps = (prob[j] >= 0.05) ? prob[j] * 0.2 : 1e-2; // within 20% if the probability is at least 5%, within 0.01 otherwise
-                ASSERT_NEAR(res, prob[j], eps) << "Difference: " << std::abs(res - prob[j]) << " Epsilon: " << eps << std::endl;
-            }
-            
-            HIP_CHECK(rocrand_destroy_discrete_distribution(discrete_distribution));
-        }
-
-        HIP_CHECK(hipFree(dInput));
-        HIP_CHECK(hipFree(dOutput));
-    
-    }
-
-    void run_alias_test(const size_t inputSize){
-        runTestInternal(
-            [] __host__ __device__ (unsigned int * output, T * input, rocrand_discrete_distribution_st &dis,  const size_t N){
-                size_t threads = 512;
-                size_t blocks = std::ceil(static_cast<double>(N) / static_cast<double>(threads));
-                hipLaunchKernelGGL(
-                    HIP_KERNEL_NAME(discrete_alias_kernel<T>),
-                    dim3(blocks),
-                    dim3(threads),
-                    0,
-                    0,
-                    output,
-                    input,
-                    dis,
-                    N
-                );
-            },
-            inputSize
-        );
-    
-    }
-
-    void run_cdf_test(const size_t inputSize){
-        runTestInternal(
-            [] __host__ __device__ (unsigned int * output, T * input, rocrand_discrete_distribution_st &dis,  const size_t N){
-                size_t threads = 512;
-                size_t blocks = std::ceil(static_cast<double>(N) / static_cast<double>(threads));
-                hipLaunchKernelGGL(
-                    HIP_KERNEL_NAME(discrete_cdf_kernel<T>),
-                    dim3(blocks),
-                    dim3(threads),
-                    0,
-                    0,
-                    output,
-                    input,
-                    dis,
-                    N
-                );
-            },
-            inputSize
-        );
-    }
-        
+template <typename DT>
+class DiscreteDistributionTests : public ::testing::Test{
+    public:
+        using T = DT;
 };
 
-TEST(discrete_distribution_tests, discrete_alias_basic){
+TYPED_TEST_SUITE(DiscreteDistributionTests, DiscreteDataType);
 
-    std::vector<double> prob = {0.1, 0.4, 0.4, 0.1};
+template<typename T, size_t items_per_thread, size_t block_size, class DiscreteFunc>
+__global__ void discrete_kernel(T * device_input, unsigned int * device_output, rocrand_discrete_distribution_st &dis, const DiscreteFunc & f){
+    constexpr size_t items_per_block = items_per_thread * block_size;
+    const size_t offset = (items_per_block * blockIdx.x) + (items_per_thread * threadIdx.x);
 
-    rocrand_discrete_distribution discrete_distribution;
-    HIP_CHECK(rocrand_create_discrete_distribution(prob.data(), prob.size(), 0, &discrete_distribution));
+    for(size_t i = 0; i < items_per_thread; i++){
+        device_output[offset + i] = f(device_input[offset + i], dis);
+    }
+}
 
 
-    size_t inputSize = 10;
-    double hInput[] = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
-    unsigned int hOutput[10];
-    double * dInput;
-    unsigned int * dOutput;
+template <typename T, class DiscreteFunc>
+void run_internal_discrete_tests(const DiscreteFunc & f){
+    constexpr size_t items_per_thread = 256;
+    constexpr size_t block_size = 32;
+    constexpr size_t items_per_block = items_per_thread * block_size;
+    constexpr size_t grid_size = 1234;
+    constexpr size_t size = grid_size * items_per_block;
 
-    HIP_CHECK(hipMalloc(&dInput, sizeof(double) * inputSize));
-    HIP_CHECK(hipMalloc(&dOutput, sizeof(unsigned int) * inputSize));
+    std::vector<std::vector<double>> all_distributions = {
+        {10, 10, 10, 10},
+        {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1},
+        {1234, 1677, 1519, 1032, 561, 254, 98, 33, 10, 2},
+        {1, 2, 8, 4, 3, 2, 1}
+    };
 
-    HIP_CHECK(hipMemcpy(dInput, hInput, sizeof(double) * inputSize, hipMemcpyHostToDevice));
+    std::random_device rd;
+    std::mt19937 gen(rd());
     
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(discrete_alias_kernel<double>),
-        dim3(1),
-        dim3(10),
-        0,
-        0,
-        dOutput,
-        dInput,
-        *discrete_distribution,
-        inputSize
+
+    T * host_input = new T[size];
+    unsigned int * host_output = new unsigned int[size];
+
+    if constexpr (std::is_same_v<T, double>){
+        std::uniform_real_distribution<double> dis(0, 1);
+        for(size_t i = 0; i < size; i++) host_input[i] = dis(gen);
+    }
+    else if constexpr(std::is_same_v<T, unsigned long> || std::is_same_v<T, unsigned int>){
+        std::uniform_int_distribution<T> dis(0, std::numeric_limits<unsigned int>::max());
+        for(size_t i = 0; i < size; i++) host_input[i] = dis(gen);
+    }
+    else{
+        std::uniform_int_distribution<T> dis(0, std::numeric_limits<T>::max());
+        for(size_t i = 0; i < size; i++) host_input[i] = dis(gen);
+    }
+
+    T * device_input;
+    unsigned int * device_output;
+
+    HIP_CHECK(hipMalloc(&device_input, sizeof(T) * size));
+    HIP_CHECK(hipMalloc(&device_output, sizeof(unsigned int) * size));
+
+    HIP_CHECK(hipMemcpy(device_input, host_input, sizeof(T) * size, hipMemcpyHostToDevice));
+
+    for(std::vector<double> distribution : all_distributions){
+        double sum = std::accumulate(distribution.begin(), distribution.end(), 0);
+        std::string error_dis;
+
+        for(const auto & x : distribution) error_dis += std::to_string(x) + " ";
+        
+        SCOPED_TRACE(testing::Message() << "With current distribution: \n" << error_dis << "\n");
+        
+        std::vector<double> expected_prob(distribution.size());
+        for(size_t i = 0; i < distribution.size(); i++)
+            expected_prob[i] = distribution[i] / sum;
+        
+        rocrand_discrete_distribution discrete_distribution;
+        ROCRAND_CHECK(rocrand_create_discrete_distribution(expected_prob.data(), expected_prob.size(), 0, &discrete_distribution));
+
+        hipLaunchKernelGGL(
+            HIP_KERNEL_NAME(discrete_kernel<T, items_per_thread, block_size>),
+            dim3(grid_size), dim3(block_size), 0, 0,
+            device_input, device_output, *discrete_distribution, f
+        );
+
+
+        HIP_CHECK(hipMemcpy(host_output, device_output, sizeof(unsigned int) * size, hipMemcpyDeviceToHost));
+
+        std::vector<double> histogram(distribution.size());
+
+        for(size_t i = 0; i < size; i++)
+            histogram[host_output[i]]++;
+
+        std::vector<double> actual_prob(distribution.size());
+        for(size_t i = 0; i < actual_prob.size(); i++)
+            actual_prob[i] = histogram[i] / static_cast<double>(size);
+        
+
+        for(size_t i = 0; i < expected_prob.size(); i++){
+            double eps = expected_prob[i] > 0.05 ? expected_prob[i] * 0.01 : 0.01;
+            ASSERT_NEAR(expected_prob[i], actual_prob[i], eps); 
+        }
+
+        ROCRAND_CHECK(rocrand_destroy_discrete_distribution(discrete_distribution));
+    }
+
+    delete [] host_input;
+    delete [] host_output;
+
+    HIP_CHECK(hipFree(device_input));
+    HIP_CHECK(hipFree(device_output));
+}
+
+TYPED_TEST(DiscreteDistributionTests, InternalDiscreteAliasTest){
+    using T = TestFixture::T;
+    run_internal_discrete_tests<T>(
+        [=] __device__(T val, rocrand_discrete_distribution_st & dis)
+            {return rocrand_device::detail::discrete_alias(val, dis);}
     );
-
-    HIP_CHECK(hipMemcpy(hOutput, dOutput, sizeof(unsigned int) * inputSize, hipMemcpyDeviceToHost));
-
-    std::vector<unsigned int> rChecker(4, 0);
-    for(size_t i = 0; i < inputSize; i++)
-        rChecker[hOutput[i]]++;
-    
-    ASSERT_EQ(rChecker[0], 1);
-    ASSERT_EQ(rChecker[1], 4);
-    ASSERT_EQ(rChecker[2], 4);
-    ASSERT_EQ(rChecker[3], 1);
-
-    HIP_CHECK(hipFree(dInput));
-    HIP_CHECK(hipFree(dOutput));
-    HIP_CHECK(rocrand_destroy_discrete_distribution(discrete_distribution));
-
-}   
-
-TEST(discrete_distribution_tests, discrete_alias_unsigned_int){
-    run_discrete_distribution_tests<unsigned int> rt;
-    rt.run_alias_test(TEST_SIZE);
 }
 
-TEST(discrete_distribution_tests, discrete_alias_unsigned_long){
-    run_discrete_distribution_tests<unsigned long> rt;
-    rt.run_alias_test(TEST_SIZE);
-}
-
-TEST(discrete_distribution_tests, discrete_alias_unsigned_long_long){
-    run_discrete_distribution_tests<unsigned long long> rt;
-    rt.run_alias_test(TEST_SIZE);
-}
-
-TEST(discrete_distribution_tests, discrete_cdf_basic){
-
-    std::vector<double> prob = {0.1, 0.4, 0.4, 0.1};
-
-    rocrand_discrete_distribution discrete_distribution;
-    HIP_CHECK(rocrand_create_discrete_distribution(prob.data(), prob.size(), 0, &discrete_distribution));
-
-    size_t inputSize = 11;
-    double hInput[] = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1};
-    unsigned int hOutput[11];
-    double * dInput;
-    unsigned int * dOutput;
-
-    HIP_CHECK(hipMalloc(&dInput, sizeof(double) * inputSize));
-    HIP_CHECK(hipMalloc(&dOutput, sizeof(unsigned int) * inputSize));
-
-    HIP_CHECK(hipMemcpy(dInput, hInput, sizeof(double) * inputSize, hipMemcpyHostToDevice));
-    
-    hipLaunchKernelGGL(HIP_KERNEL_NAME(discrete_cdf_kernel<double>),
-        dim3(1),
-        dim3(inputSize),
-        0,
-        0,
-        dOutput,
-        dInput,
-        *discrete_distribution,
-        inputSize
+TYPED_TEST(DiscreteDistributionTests, InternalDiscreteCDFTest){
+    using T = TestFixture::T;
+    run_internal_discrete_tests<T>(
+        [=] __device__(T val, rocrand_discrete_distribution_st & dis)
+            {return rocrand_device::detail::discrete_cdf(val, dis);}
     );
-
-    HIP_CHECK(hipMemcpy(hOutput, dOutput, sizeof(unsigned int) * inputSize, hipMemcpyDeviceToHost));
-
-    std::vector<unsigned int> rChecker(4, 0);
-    for(size_t i = 0; i < inputSize; i++)
-        rChecker[hOutput[i]]++;
-    
-
-    ASSERT_EQ(rChecker[0], 2);
-    ASSERT_EQ(rChecker[1], 4);
-    ASSERT_EQ(rChecker[2], 4);
-    ASSERT_EQ(rChecker[3], 1);
-
-    HIP_CHECK(hipFree(dInput));
-    HIP_CHECK(hipFree(dOutput));
-    HIP_CHECK(rocrand_destroy_discrete_distribution(discrete_distribution));
-
-}
-
-TEST(discrete_distribution_tests, discrete_cdf_unsigned_int){
-    run_discrete_distribution_tests<unsigned int> rt;
-    rt.run_cdf_test(TEST_SIZE);
-}
-
-TEST(discrete_distribution_tests, discrete_cdf_unsigned_long){
-    run_discrete_distribution_tests<unsigned long> rt;
-    rt.run_cdf_test(TEST_SIZE);
-}
-
-TEST(discrete_distribution_tests, discrete_cdf_unsigned_long_long){
-    run_discrete_distribution_tests<unsigned long long> rt;
-    rt.run_cdf_test(TEST_SIZE);
 }
