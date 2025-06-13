ddb644dcec6b2129bb0afc4ca266d8dc742d06ae
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-13 10:03:32 +0000

    Deprecate public CUB/Thrust deprecation macros

--- projects/rocthrust/thrust/detail/config/deprecated.h
+++ projects/rocthrust/thrust/detail/config/deprecated.h
@@ -20,6 +20,16 @@
 
 #pragma once
 
+// Internal config header that is only included through thrust/detail/config/config.h
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
 #include <thrust/detail/config/compiler.h>
 #include <thrust/detail/config/cpp_dialect.h>
 
@@ -30,21 +40,33 @@
 #define THRUST_ALIAS_ATTRIBUTE(...)
 
 #ifdef THRUST_IGNORE_DEPRECATED_API
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG)
 #elif THRUST_CPP_DIALECT >= 2017
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED              [[deprecated]]
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG) [[deprecated(MSG)]]
 #elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_MSVC
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED              __declspec(deprecated)
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG) __declspec(deprecated(MSG))
 #elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_CLANG
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED              __attribute__((deprecated))
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG) __attribute__((deprecated(MSG)))
 #elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_GCC
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED              __attribute__((deprecated))
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG) __attribute__((deprecated(MSG)))
 #else
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED
+//! deprecated [Since 2.8]
 #  define THRUST_DEPRECATED_BECAUSE(MSG)
 #endif
