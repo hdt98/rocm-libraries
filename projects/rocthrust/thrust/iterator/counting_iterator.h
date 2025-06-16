024a6c9fc059854640a7d00659a8a3c85715fb80
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-16 13:55:15 +0000

    Fix Thrust/CUB tests by adding empty base opt-ins to iterator classes

--- projects/rocthrust/thrust/iterator/counting_iterator.h
+++ projects/rocthrust/thrust/iterator/counting_iterator.h
@@ -32,6 +32,13 @@
 
 #include <thrust/detail/config.h>
 
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
 #include <thrust/iterator/iterator_adaptor.h>
 #include <thrust/iterator/iterator_categories.h>
 #include <thrust/iterator/iterator_facade.h>
@@ -128,7 +135,12 @@ template <typename Incrementable,
           typename System     = use_default,
           typename Traversal  = use_default,
           typename Difference = use_default>
-class counting_iterator : public detail::counting_iterator_base<Incrementable, System, Traversal, Difference>::type
+#if defined(_WIN32) && THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_MSVC
+class __declspec(empty_bases) counting_iterator
+#else
+class counting_iterator
+#endif
+    : public detail::counting_iterator_base<Incrementable, System, Traversal, Difference>::type
 {
   /*! \cond
    */
