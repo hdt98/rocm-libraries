7dfef9d531a5408185af9a7cc8ec65d3caeaea8d
Lin, Ed <Ed.Lin@amd.com>
2025-07-10 14:36:40 +0800

    [hipSPARSELt] Enable sparse b kernels for fp16 and bf16 datatypes (#32)
    * fix gfx1250 sparse offset.
    
    * fix kIncB for block offset within cndmask in tail loop.
    
    * Fix offset and missing vgprPackTemp for DepthU and TailLoop
    
    * Add correct mask for metadata in tail loop.
    
    * Update spmm_f16 and spmm_f16_sb yamls.
    
    * Update f16 yamls and add bf16 yamls.
    
    * Fix and update sparse gfx1250 yamls.
    
    * *Workaround metadataGPR*: should be taken out when ROCm build is updated.
    
    * Update sparse testing yamls.
    
    ---------
    
    Co-authored-by: edlin102 <edlin102@amd.com>
    Co-authored-by: Vin Huang <vin.huang@amd.com>

--- projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
@@ -5473,10 +5473,10 @@ class KernelWriter(metaclass=abc.ABCMeta):
         vgprIdx = self.states.b.startVgprValu \
             + max(self.states.b.numVgprValu + numVgprValuPackB, self.states.b.numVgprG2LAllocated)
 
-    if ((tensorParametersA["bpe"] < 4 and not kernel["UnrollMajorLDSA"]) or                                                      \
-        (tensorParametersB["bpe"] < 4 and not kernel["UnrollMajorLDSB"]) or                                                      \
-        (kernel["ProblemType"]["Sparse"] and not kernel["UnrollMajorLDSMetadata"] and kernel["MIInputPerThreadMetadata"] == 4))  \
-        and (kernel["ProblemType"]["DataType"].isInt8() or kernel["ProblemType"]["DataType"].is8bitFloat()):
+    if (((tensorParametersA["bpe"] < 4 and not kernel["UnrollMajorLDSA"]) or                                 \
+         (tensorParametersB["bpe"] < 4 and not kernel["UnrollMajorLDSB"]))                                   \
+        and (kernel["ProblemType"]["DataType"].isInt8() or kernel["ProblemType"]["DataType"].is8bitFloat())) \
+       or (kernel["ProblemType"]["Sparse"] and not kernel["UnrollMajorLDSMetadata"] and kernel["MIInputPerThreadMetadata"] > 1) :
       self.states.a.startVgprValuPackTemp = vgprIdx
       self.states.b.startVgprValuPackTemp = vgprIdx
       vgprIdx += 1
