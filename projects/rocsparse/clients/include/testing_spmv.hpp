7d7bfe8e22e16c49ac4b97d02d94ff1f13e537a7
jsandham <jsandham@amd.com>
2025-08-19 21:58:16 +0000

    Testing

--- projects/rocsparse/clients/include/testing_spmv.hpp
+++ projects/rocsparse/clients/include/testing_spmv.hpp
@@ -296,17 +296,57 @@ public:
         int                 m;
         int                 n;
         int                 nnz;
-        std::vector<int>    hcoo_row_ind;
-        std::vector<int>    hcoo_col_ind;
-        std::vector<double> hcoo_val;
+        std::vector<int>    coo_row_ind;
+        std::vector<int>    coo_col_ind;
+        std::vector<double> coo_val;
 
         load_mtx_file_coo("../../../../../../../shipsec1/shipsec1.mtx",
-                          hcoo_row_ind,
-                          hcoo_col_ind,
-                          hcoo_val,
+                          coo_row_ind,
+                          coo_col_ind,
+                          coo_val,
                           m,
                           n,
                           nnz);
+
+	int* hcoo_row_ind = nullptr;
+	int* hcoo_col_ind = nullptr;
+	double* hcoo_val = nullptr;
+	HIP_CHECK(hipHostMalloc(&hcoo_row_ind, sizeof(int) * nnz));
+	HIP_CHECK(hipHostMalloc(&hcoo_col_ind, sizeof(int) * nnz));
+	HIP_CHECK(hipHostMalloc(&hcoo_val, sizeof(double) * nnz));
+
+	for(int i = 0; i < nnz; i++)
+	{
+	    hcoo_row_ind[i] = coo_row_ind[i];
+	    hcoo_col_ind[i] = coo_col_ind[i];
+	    hcoo_val[i] = coo_val[i];
+	}
+
+	int M;
+	int N;
+	host_coo_matrix<double> hA;
+        rocsparse_matrix_factory<double> matrix_factory(arg);
+        matrix_factory.init_coo(hA, M, N);
+	//bool passed = true;
+	//for(int i = 0; i < hA.nnz; i++)
+	//{
+	//    if(hA.row_ind[i] != hcoo_row_ind[i])
+	//    {
+	//	passed = false;
+	//	break;
+	//    }
+	//    if(hA.col_ind[i] != hcoo_col_ind[i])
+        //    {
+        //        passed = false;
+        //        break;
+        //    }
+	//    if(hA.val[i] != hcoo_val[i])
+        //    {
+        //        passed = false;
+        //        break;
+        //    }
+	//}
+	//std::cout << "passed: " << passed << std::endl;
 
         std::cout << "m: " << m << " n: " << n << " nnz: " << nnz << std::endl;
 
@@ -325,11 +365,11 @@ public:
         HIP_CHECK(hipMalloc((void**)&dy, sizeof(double) * m));
 
         HIP_CHECK(
-            hipMemcpy(dcoo_row_ind, hcoo_row_ind.data(), sizeof(int) * nnz, hipMemcpyHostToDevice));
+            hipMemcpy(dcoo_row_ind, hcoo_row_ind, sizeof(int) * nnz, hipMemcpyHostToDevice));
         HIP_CHECK(
-            hipMemcpy(dcoo_col_ind, hcoo_col_ind.data(), sizeof(int) * nnz, hipMemcpyHostToDevice));
+            hipMemcpy(dcoo_col_ind, hcoo_col_ind, sizeof(int) * nnz, hipMemcpyHostToDevice));
         HIP_CHECK(
-            hipMemcpy(dcoo_val, hcoo_val.data(), sizeof(double) * nnz, hipMemcpyHostToDevice));
+            hipMemcpy(dcoo_val, hcoo_val, sizeof(double) * nnz, hipMemcpyHostToDevice));
         HIP_CHECK(hipMemcpy(dx, hx.data(), sizeof(double) * n, hipMemcpyHostToDevice));
         HIP_CHECK(hipMemcpy(dy, hy.data(), sizeof(double) * m, hipMemcpyHostToDevice));
 
@@ -351,146 +391,8 @@ public:
         HIP_CHECK(hipFree(dx));
         HIP_CHECK(hipFree(dy));
 
-        /*int                  M;
-        int                  N;
-        rocsparse_operation  trans = rocsparse_operation_none;
-        rocsparse_index_base base  = rocsparse_index_base_zero;
-        rocsparse_datatype   ttype = rocsparse_datatype_f64_r;
-
-        double alpha = 1.0;
-        double beta  = 0.0;
-
-        rocsparse_handle      handle;
-        rocsparse_spmat_descr matA;
-        rocsparse_dnvec_descr vecX;
-        rocsparse_dnvec_descr vecY;
-
-        rocsparse_create_handle(&handle);
-
-        host_coo_matrix<double> hA;
-
-        rocsparse_matrix_factory<double> matrix_factory(arg);
-        matrix_factory.init_coo(hA, M, N);
-
-        std::cout << "hA.m: " << hA.m << " hA.n: " << hA.n << " hA.nnz: " << hA.nnz << std::endl;
-
-        //std::cout << "hA.row_ind" << std::endl;
-        //for(int i = 0; i < 1000; i++)
-        //{
-        //    std::cout << hA.row_ind[i] << " ";
-        //}
-        //std::cout << "" << std::endl;
-
-        //std::cout << "hA.col_ind" << std::endl;
-        //for(int i = 0; i < 1000; i++)
-        //{
-        //    std::cout << hA.col_ind[i] << " ";
-        //}
-        //std::cout << "" << std::endl;
-
-        host_dense_matrix<double> hx(N, 1);
-        host_dense_matrix<double> hy(M, 1);
-        for(int i = 0; i < N; i++)
-        {
-            hx[i] = 1.0;
-        }
-        for(int i = 0; i < M; i++)
-        {
-            hy[i] = 1.0;
-        }
-
-        device_coo_matrix<double>   dA(hA);
-        device_dense_matrix<double> dx(hx), dy(hy);
-
-        hipMemset(dy, 0, sizeof(double) * M);
-
-        // Create sparse matrix A
-        rocsparse_create_coo_descr(&matA,
-                                   dA.m,
-                                   dA.n,
-                                   dA.nnz,
-                                   dA.row_ind,
-                                   dA.col_ind,
-                                   dA.val,
-                                   get_indextype<int>(),
-                                   base,
-                                   ttype);
-
-        // Create dense vector X
-        rocsparse_create_dnvec_descr(&vecX, N, dx, ttype);
-
-        // Create dense vector Y
-        rocsparse_create_dnvec_descr(&vecY, M, dy, ttype);
-
-        // Call spmv to perform computation
-        size_t buffer_size = 0;
-        void*  temp_buffer = nullptr;
-        rocsparse_spmv(handle,
-                       trans,
-                       &alpha,
-                       matA,
-                       vecX,
-                       &beta,
-                       vecY,
-                       ttype,
-                       rocsparse_spmv_alg_coo_atomic,
-                       rocsparse_spmv_stage_compute,
-                       &buffer_size,
-                       temp_buffer);
-
-        // Clear rocSPARSE
-        rocsparse_destroy_spmat_descr(matA);
-        rocsparse_destroy_dnvec_descr(vecX);
-        rocsparse_destroy_dnvec_descr(vecY);
-        rocsparse_destroy_handle(handle);*/
-
-        /*J                      M           = arg.M;
-        J                      N           = arg.N;
-        rocsparse_operation    trans       = arg.transA;
-        rocsparse_index_base   base        = arg.baseA;
-        rocsparse_spmv_alg     alg         = arg.spmv_alg;
-        rocsparse_datatype     ttype       = get_datatype<T>();
-
-	T h_alpha = arg.get_alpha<T>();
-	T h_beta = arg.get_beta<T>();
-
-        // Create rocsparse handle
-        rocsparse_local_handle handle(arg);
-
-        // INITIALIZATE THE SPARSE MATRIX
-        host_sparse_matrix<A> hA;
-        rocsparse_matrix_factory<A, I, J> matrix_factory(arg);
-	traits::sparse_initialization(matrix_factory, hA, M, N, base);
-
-        device_sparse_matrix<A> dA(hA);
-
-        host_dense_matrix<X> hx(N, 1);
-        for(int i = 0; i < N; i++)
-	{
-	    hx[i] = 1.0;
-	}
-	device_dense_matrix<X> dx(hx);
-
-        host_dense_matrix<Y> hy(M, 1);
-        for(int i = 0; i < M; i++)
-        {
-            hy[i] = 1.0;
-        }
-	device_dense_matrix<Y> dy(hy);
-
-        rocsparse_local_spmat matA(dA);
-        rocsparse_local_dnvec x(dx);
-        rocsparse_local_dnvec y(dy);
-
-	CHECK_HIP_ERROR(hipDeviceSynchronize());
-        std::cout << "Before host mode compute" << std::endl;
-
-        // Run buffer size
-        void*  dbuffer     = nullptr;
-        size_t buffer_size = 0;
-        CHECK_ROCSPARSE_ERROR(rocsparse_spmv(handle, trans, &h_alpha, matA, x, &h_beta, y, ttype, alg, rocsparse_spmv_stage_compute, &buffer_size, dbuffer));
-
-        CHECK_HIP_ERROR(hipDeviceSynchronize());
-        std::cout << "Made it passed host mode compute" << std::endl;*/
+	HIP_CHECK(hipHostFree(hcoo_row_ind));
+	HIP_CHECK(hipHostFree(hcoo_col_ind));
+	HIP_CHECK(hipHostFree(hcoo_val));
     }
 };
