0c3fc9ad39d8005aeb56d41c3ea69dbd2e0fc387
jsandham <jsandham@amd.com>
2025-08-21 21:34:03 +0000

    Fix sparse_to_sparse documentation example

--- projects/rocsparse/clients/samples/documentation_examples/generic/example_rocsparse_sparse_to_sparse.cpp
+++ projects/rocsparse/clients/samples/documentation_examples/generic/example_rocsparse_sparse_to_sparse.cpp
@@ -48,54 +48,148 @@
 //! [doc example]
 int main()
 {
-    // // It assumes the CSR arrays (ptr, ind, val) have already been allocated and filled.
-    // // Build Source
-    // rocsparse_spmat_descr source;
-    // ROCSPARSE_CHECK(rocsparse_create_csr_descr(&source, M, N, nnz, ptr, ind, val, rocsparse_indextype_i32, rocsparse_indextype_i32, rocsparse_index_base_zero, rocsparse_datatype_f32_r));
+    // 4 2 0 1 0
+    // 2 4 2 0 1
+    // 0 2 4 2 0
+    // 1 0 2 4 2
+    // 0 1 0 2 4
+    int m   = 5;
+    int n   = 5;
+    int nnz = 17;
 
-    // // Build target
-    // void * ell_ind, * ell_val;
-    // int64_t ell_width = 0;
-    // rocsparse_spmat_descr target;
-    // ROCSPARSE_CHECK(rocsparse_create_ell_descr(&target, M, N, ell_ind, ell_val, ell_width, rocsparse_indextype_i32, rocsparse_index_base_zero, rocsparse_datatype_f32_r));
+    std::vector<int>    hcsr_row_ptr = {0, 3, 7, 10, 14, 17};
+    std::vector<int>    hcsr_col_ind = {0, 1, 3, 0, 1, 2, 4, 1, 2, 3, 0, 2, 3, 4, 1, 3, 4};
+    std::vector<double> hcsr_val
+        = {4.0, 2.0, 1.0, 2.0, 4.0, 2.0, 1.0, 2.0, 4.0, 2.0, 1.0, 2.0, 4.0, 2.0, 1.0, 2.0, 4.0};
 
-    // // Create descriptor
-    // rocsparse_sparse_to_sparse_descr descr;
-    // ROCSPARSE_CHECK(rocsparse_create_sparse_to_sparse_descr(&descr, source, target,  rocsparse_sparse_to_sparse_alg_default));
+    // rocSPARSE handle
+    rocsparse_handle handle;
+    ROCSPARSE_CHECK(rocsparse_create_handle(&handle));
 
-    // // Analysis phase
-    // ROCSPARSE_CHECK(rocsparse_sparse_to_sparse_buffer_size(handle, descr, source, target, rocsparse_sparse_to_sparse_stage_analysis, &buffer_size));
-    // HIP_CHECK(hipMalloc(&buffer,buffer_size));
-    // ROCSPARSE_CHECK(rocsparse_sparse_to_sparse(handle, descr, source, target, rocsparse_sparse_to_sparse_stage_analysis, buffer_size, buffer));
-    // HIP_CHECK(hipFree(buffer));
+    int*    dcsr_row_ptr = nullptr;
+    int*    dcsr_col_ind = nullptr;
+    double* dcsr_val     = nullptr;
+    HIP_CHECK(hipMalloc((void**)&dcsr_row_ptr, sizeof(int) * (m + 1)));
+    HIP_CHECK(hipMalloc((void**)&dcsr_col_ind, sizeof(int) * nnz));
+    HIP_CHECK(hipMalloc((void**)&dcsr_val, sizeof(double) * nnz));
 
-    // // the user is responsible to allocate target arrays after the analysis phase.
-    // {
-    //     int64_t rows, cols, ell_width;
-    //     void * ind, * val;
-    //     rocsparse_indextype        idx_type;
-    //     rocsparse_index_base       idx_base;
-    //     rocsparse_datatype         data_type;
+    HIP_CHECK(
+        hipMemcpy(dcsr_row_ptr, hcsr_row_ptr.data(), sizeof(int) * (m + 1), hipMemcpyHostToDevice));
+    HIP_CHECK(
+        hipMemcpy(dcsr_col_ind, hcsr_col_ind.data(), sizeof(int) * nnz, hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(dcsr_val, hcsr_val.data(), sizeof(double) * nnz, hipMemcpyHostToDevice));
 
-    //     ROCSPARSE_CHECK(rocsparse_ell_get(target,
-    //                     &rows,
-    //                     &cols,
-    //                     &ind,
-    //                     &val,
-    //                     &ell_width,
-    //                     &idx_type,
-    //                     &idx_base,
-    //                     &data_type));
-    //     HIP_CHECK(hipMalloc(&ell_ind,ell_width * M * sizeof(int32_t)));
-    //     HIP_CHECK(hipMalloc(&ell_val,ell_width * M * sizeof(float)));
-    //     ROCSPARSE_CHECK(rocsparse_ell_set_pointers(target, ell_ind, ell_val));
-    // }
+    // It assumes the CSR arrays (ptr, ind, val) have already been allocated and filled.
+    // Build Source
+    rocsparse_spmat_descr source;
+    ROCSPARSE_CHECK(rocsparse_create_csr_descr(&source,
+                                               m,
+                                               n,
+                                               nnz,
+                                               dcsr_row_ptr,
+                                               dcsr_col_ind,
+                                               dcsr_val,
+                                               rocsparse_indextype_i32,
+                                               rocsparse_indextype_i32,
+                                               rocsparse_index_base_zero,
+                                               rocsparse_datatype_f64_r));
 
-    // // Calculation phase
-    // ROCSPARSE_CHECK(rocsparse_sparse_to_sparse_buffer_size(handle, descr, source, target, rocsparse_sparse_to_sparse_stage_compute, &buffer_size));
-    // HIP_CHECK(hipMalloc(&buffer,buffer_size));
-    // ROCSPARSE_CHECK(rocsparse_sparse_to_sparse(handle, descr, source, target, rocsparse_sparse_to_sparse_stage_compute, buffer_size, buffer));
-    // HIP_CHECK(hipFree(buffer));
+    // Build target
+    void *                dell_ind, *dell_val;
+    int64_t               ell_width = 0;
+    rocsparse_spmat_descr target;
+    ROCSPARSE_CHECK(rocsparse_create_ell_descr(&target,
+                                               m,
+                                               n,
+                                               dell_ind,
+                                               dell_val,
+                                               ell_width,
+                                               rocsparse_indextype_i32,
+                                               rocsparse_index_base_zero,
+                                               rocsparse_datatype_f64_r));
+
+    // Create descriptor
+    rocsparse_sparse_to_sparse_descr descr;
+    ROCSPARSE_CHECK(rocsparse_create_sparse_to_sparse_descr(
+        &descr, source, target, rocsparse_sparse_to_sparse_alg_default));
+
+    // Analysis phase
+    size_t buffer_size;
+    ROCSPARSE_CHECK(rocsparse_sparse_to_sparse_buffer_size(
+        handle, descr, source, target, rocsparse_sparse_to_sparse_stage_analysis, &buffer_size));
+
+    void* buffer = nullptr;
+    HIP_CHECK(hipMalloc(&buffer, buffer_size));
+
+    ROCSPARSE_CHECK(rocsparse_sparse_to_sparse(handle,
+                                               descr,
+                                               source,
+                                               target,
+                                               rocsparse_sparse_to_sparse_stage_analysis,
+                                               buffer_size,
+                                               buffer));
+    HIP_CHECK(hipFree(buffer));
+
+    // the user is responsible to allocate target arrays after the analysis phase.
+    int64_t              rows, cols;
+    void *               ind, *val;
+    rocsparse_indextype  idx_type;
+    rocsparse_index_base idx_base;
+    rocsparse_datatype   data_type;
+
+    // Get ell_width
+    ROCSPARSE_CHECK(rocsparse_ell_get(
+        target, &rows, &cols, &ind, &val, &ell_width, &idx_type, &idx_base, &data_type));
+
+    std::cout << "rows: " << rows << " cols: " << cols << " ell_width: " << ell_width << std::endl;
+
+    // Allocate device arrays for ELL format
+    HIP_CHECK(hipMalloc(&dell_ind, sizeof(int) * ell_width * m));
+    HIP_CHECK(hipMalloc(&dell_val, sizeof(double) * ell_width * m));
+
+    ROCSPARSE_CHECK(rocsparse_ell_set_pointers(target, dell_ind, dell_val));
+
+    // Calculation phase
+    ROCSPARSE_CHECK(rocsparse_sparse_to_sparse_buffer_size(
+        handle, descr, source, target, rocsparse_sparse_to_sparse_stage_compute, &buffer_size));
+    HIP_CHECK(hipMalloc(&buffer, buffer_size));
+    ROCSPARSE_CHECK(rocsparse_sparse_to_sparse(handle,
+                                               descr,
+                                               source,
+                                               target,
+                                               rocsparse_sparse_to_sparse_stage_compute,
+                                               buffer_size,
+                                               buffer));
+    HIP_CHECK(hipFree(buffer));
+
+    std::vector<int>    hell_ind(ell_width * m);
+    std::vector<double> hell_val(ell_width * m);
+
+    HIP_CHECK(
+        hipMemcpy(hell_ind.data(), dell_ind, sizeof(int) * ell_width * m, hipMemcpyDeviceToHost));
+    HIP_CHECK(hipMemcpy(
+        hell_val.data(), dell_val, sizeof(double) * ell_width * m, hipMemcpyDeviceToHost));
+
+    std::cout << "hell_ind" << std::endl;
+    for(size_t i = 0; i < hell_ind.size(); i++)
+    {
+        std::cout << hell_ind[i] << " ";
+    }
+    std::cout << "" << std::endl;
+
+    std::cout << "hell_val" << std::endl;
+    for(size_t i = 0; i < hell_val.size(); i++)
+    {
+        std::cout << hell_val[i] << " ";
+    }
+    std::cout << "" << std::endl;
+
+    HIP_CHECK(hipFree(dcsr_row_ptr));
+    HIP_CHECK(hipFree(dcsr_col_ind));
+    HIP_CHECK(hipFree(dcsr_val));
+
+    HIP_CHECK(hipFree(dell_ind));
+    HIP_CHECK(hipFree(dell_val));
 
     return 0;
 }
