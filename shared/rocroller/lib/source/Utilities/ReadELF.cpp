ab5d5df9bbbd09699184f6b8b23a793d05cb0ce8
isaacbuckingham <isaacbuckingham5@gmail.com>
2025-08-25 13:14:53 -0500

    ReadELF reads info using comgr

--- shared/rocroller/lib/source/Utilities/ReadELF.cpp
+++ shared/rocroller/lib/source/Utilities/ReadELF.cpp
@@ -23,95 +23,191 @@
  * SOFTWARE.
  *
  *******************************************************************************/
-#ifdef ROCROLLER_USE_LLD
-#include <llvm/BinaryFormat/AMDGPUMetadataVerifier.h>
-#include <llvm/BinaryFormat/MsgPackDocument.h>
-#include <llvm/Object/ELF.h>
-#include <llvm/Object/ELFObjectFile.h>
-#include <llvm/Object/ObjectFile.h>
-#include <llvm/Support/AMDGPUMetadata.h>
-#include <llvm/Support/Error.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/Support/raw_ostream.h>
-using namespace llvm;
-using namespace llvm::object;
-#endif
-
 #include <rocRoller/Utilities/Error.hpp>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include <amd_comgr/amd_comgr.h>
 
 using namespace rocRoller;
 
 std::string rocRoller::readMetaDataFromCodeObject(std::string const& fileName)
 {
     std::string yaml;
-#ifdef ROCROLLER_USE_LLD
-    auto maybeBuffer = MemoryBuffer::getFile(fileName);
-    if(!maybeBuffer)
+    // Read the file into memory
+    std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+    if(!file.is_open())
     {
-        Throw<FatalError>("Error reading file: ", maybeBuffer.getError().message());
+        Throw<FatalError>("Error opening file: ", fileName);
     }
-
-    auto maybeELFObject = ObjectFile::createELFObjectFile(maybeBuffer->get()->getMemBufferRef());
-    if(!maybeELFObject)
+    
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    
+    std::vector<char> buffer(size);
+    if(!file.read(buffer.data(), size))
     {
-        Throw<FatalError>("Error creating ELF object file: ", toString(maybeELFObject.takeError()));
+        Throw<FatalError>("Error reading file: ", fileName);
     }
-
-    auto elf64LE = dyn_cast<ELF64LEObjectFile>(maybeELFObject->get());
-    if(!elf64LE)
+    file.close();
+    
+    // Create comgr data object
+    amd_comgr_data_t dataIn;
+    amd_comgr_status_t status = amd_comgr_create_data(AMD_COMGR_DATA_KIND_RELOCATABLE, &dataIn);
+    if(status != AMD_COMGR_STATUS_SUCCESS)
     {
-        Throw<FatalError>("Not an ELF file: ", fileName);
+        const char* statusString;
+        amd_comgr_status_string(status, &statusString);
+        Throw<FatalError>("Failed to create data object: ", statusString);
     }
-    auto elf = elf64LE->getELFFile();
-
-    auto maybeSections = elf.sections();
-    if(!maybeSections)
+    
+    // Set the data
+    status = amd_comgr_set_data(dataIn, size, buffer.data());
+    if(status != AMD_COMGR_STATUS_SUCCESS)
     {
-        Throw<FatalError>("Can not read ELF sections.");
+        const char* statusString;
+        amd_comgr_status_string(status, &statusString);
+        amd_comgr_release_data(dataIn);
+        Throw<FatalError>("Failed to set data: ", statusString);
     }
-
-    auto sections = *maybeSections;
-    for(const auto& section : sections)
+    
+    // Get metadata from data object
+    amd_comgr_metadata_node_t meta;
+    status = amd_comgr_get_data_metadata(dataIn, &meta);
+    if(status != AMD_COMGR_STATUS_SUCCESS)
     {
-        if(section.sh_type != ELF::SHT_NOTE)
-            continue;
-
-        auto err = llvm::Error::success();
-        for(auto note : elf.notes(section, err))
-        {
-            if(note.getName() != "AMDGPU")
-                continue;
-
-            auto type = note.getType();
-            if(type != ELF::NT_AMDGPU_METADATA)
-                continue;
-
-#if LLVM_VERSION_MAJOR >= 18
-            // See https://llvm.org/docs/AMDGPUUsage.html#note-records for note re: alignment
-            auto desc = note.getDesc(4);
-#else
-            auto desc = note.getDesc();
-#endif
-            auto msg = StringRef(reinterpret_cast<const char*>(desc.data()), desc.size());
-
-            msgpack::Document meta;
-            if(!meta.readFromBlob(msg, false))
-                continue;
-
-            AMDGPU::HSAMD::V3::MetadataVerifier Verifier(true);
-            if(!Verifier.verify(meta.getRoot()))
+        const char* statusString;
+        amd_comgr_status_string(status, &statusString);
+        amd_comgr_release_data(dataIn);
+        Throw<FatalError>("Failed to get metadata: ", statusString);
+    }
+    
+    // Check if we have valid metadata
+    amd_comgr_metadata_kind_t mkind;
+    status = amd_comgr_get_metadata_kind(meta, &mkind);
+    if(status == AMD_COMGR_STATUS_SUCCESS && mkind != AMD_COMGR_METADATA_KIND_NULL)
+    {
+        // Convert metadata to YAML by iterating through the map
+        // Since comgr doesn't provide a direct toYAML function, we'll need to
+        // manually build the YAML string by traversing the metadata structure
+        std::ostringstream yamlStream;
+        
+        // Helper lambda to convert metadata to YAML format
+        std::function<void(amd_comgr_metadata_node_t, int)> metadataToYaml;
+        metadataToYaml = [&](amd_comgr_metadata_node_t node, int indent) {
+            amd_comgr_metadata_kind_t kind;
+            if(amd_comgr_get_metadata_kind(node, &kind) != AMD_COMGR_STATUS_SUCCESS)
+                return;
+                
+            std::string indentStr(indent * 2, ' ');
+            
+            switch(kind)
             {
-                Throw<FatalError>("Invalid AMDGPU::HSAMD metadata");
+                case AMD_COMGR_METADATA_KIND_STRING:
+                {
+                    size_t size = 0;
+                    amd_comgr_get_metadata_string(node, &size, nullptr);
+                    std::vector<char> str(size);
+                    amd_comgr_get_metadata_string(node, &size, str.data());
+                    yamlStream << str.data();
+                    break;
+                }
+                case AMD_COMGR_METADATA_KIND_MAP:
+                {
+                    struct MapContext {
+                        std::ostringstream* stream;
+                        std::function<void(amd_comgr_metadata_node_t, int)>* converter;
+                        int indent;
+                        bool first;
+                    };
+                    
+                    MapContext ctx = { &yamlStream, &metadataToYaml, indent, true };
+                    
+                    amd_comgr_iterate_map_metadata(node, 
+                        [](amd_comgr_metadata_node_t key, amd_comgr_metadata_node_t value, void* user_data) -> amd_comgr_status_t {
+                            MapContext* ctx = static_cast<MapContext*>(user_data);
+                            
+                            if(!ctx->first)
+                                *(ctx->stream) << "\n";
+                            ctx->first = false;
+                            
+                            std::string indentStr(ctx->indent * 2, ' ');
+                            *(ctx->stream) << indentStr;
+                            
+                            // Output key
+                            size_t keySize = 0;
+                            amd_comgr_get_metadata_string(key, &keySize, nullptr);
+                            std::vector<char> keyStr(keySize);
+                            amd_comgr_get_metadata_string(key, &keySize, keyStr.data());
+                            *(ctx->stream) << keyStr.data() << ": ";
+                            
+                            // Check value kind
+                            amd_comgr_metadata_kind_t valueKind;
+                            amd_comgr_get_metadata_kind(value, &valueKind);
+                            
+                            if(valueKind == AMD_COMGR_METADATA_KIND_MAP || valueKind == AMD_COMGR_METADATA_KIND_LIST)
+                            {
+                                *(ctx->stream) << "\n";
+                                (*ctx->converter)(value, ctx->indent + 1);
+                            }
+                            else
+                            {
+                                (*ctx->converter)(value, 0);
+                            }
+                            
+                            return AMD_COMGR_STATUS_SUCCESS;
+                        }, &ctx);
+                    break;
+                }
+                case AMD_COMGR_METADATA_KIND_LIST:
+                {
+                    size_t listSize = 0;
+                    amd_comgr_get_metadata_list_size(node, &listSize);
+                    
+                    for(size_t i = 0; i < listSize; ++i)
+                    {
+                        yamlStream << indentStr << "- ";
+                        
+                        amd_comgr_metadata_node_t item;
+                        if(amd_comgr_index_list_metadata(node, i, &item) == AMD_COMGR_STATUS_SUCCESS)
+                        {
+                            amd_comgr_metadata_kind_t itemKind;
+                            amd_comgr_get_metadata_kind(item, &itemKind);
+                            
+                            if(itemKind == AMD_COMGR_METADATA_KIND_MAP || itemKind == AMD_COMGR_METADATA_KIND_LIST)
+                            {
+                                yamlStream << "\n";
+                                metadataToYaml(item, indent + 1);
+                            }
+                            else
+                            {
+                                metadataToYaml(item, 0);
+                            }
+                            
+                            amd_comgr_destroy_metadata(item);
+                        }
+                        
+                        if(i < listSize - 1)
+                            yamlStream << "\n";
+                    }
+                    break;
+                }
+                default:
+                    break;
             }
-
-            raw_string_ostream os(yaml);
-            meta.toYAML(os);
-
-            // Currently we're only looking at the first note...
-            break;
+        };
+        
+        if(mkind == AMD_COMGR_METADATA_KIND_MAP)
+        {
+            metadataToYaml(meta, 0);
+            yaml = yamlStream.str();
         }
     }
-#endif
+    
+    // Cleanup
+    amd_comgr_destroy_metadata(meta);
+    amd_comgr_release_data(dataIn);
 
     return yaml;
 }
