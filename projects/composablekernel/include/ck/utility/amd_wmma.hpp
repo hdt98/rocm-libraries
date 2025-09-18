35fcbf548119cb788b84ccf022dca8f999ad5454
Kenneth O'Neal <Kenneth.O'Neal@amd.com>
2025-09-18 22:26:55 +0000

    remove gfx13 reference, rename gfx_120 to gfx12 in amd_wmma

--- projects/composablekernel/include/ck/utility/amd_wmma.hpp
+++ projects/composablekernel/include/ck/utility/amd_wmma.hpp
@@ -14,8 +14,9 @@ namespace ck {
 #define __gfx11__
 #endif
 
-#if defined(__gfx1200__) || defined(__gfx1201__) || defined(__gfx12_generic__)
-#define __gfx120__
+#if defined(__gfx1200__) || defined(__gfx1201__) || defined(__gfx12_generic__) \
+    defined(__gfx1250__) || defined(__gfx1251__)
+#define __gfx12__
 #endif
 
 /********************************WAVE32 MODE***********************************************/
@@ -104,7 +105,7 @@ struct intrin_wmma_bf16_16x16x16_bf16_w32<16, 16, Opsel>
         // opsel usage
         // false: D0.[0:15] = result
         // true : D0.[16:31]= result
-#if defined(__gfx11__) || defined(__gfx120__)
+#if defined(__gfx11__) || defined(__gfx12__)
         reg_c.template AsType<bhalf16_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_bf16_16x16x16_bf16_w32(
                 reg_a, reg_b, reg_c.template AsType<bhalf16_t>()[Number<0>{}], Opsel);
@@ -280,7 +281,7 @@ struct intrin_wmma_f32_16x16x16_f16_w32_gfx12<16, 16>
         // delete them.
         // amd_assembly_wmma_f32_16x16x16_f16_w32(
         //     reg_a, reg_b, reg_c.template AsType<float8_t>()(Number<0>{}));
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_f16_w32_gfx12(
                 reg_a, reg_b, reg_c.template AsType<float8_t>()[Number<0>{}]);
@@ -302,7 +303,7 @@ struct intrin_wmma_f32_16x16x16_bf16_w32_gfx12<16, 16>
     template <class FloatC>
     __device__ static void Run(const bhalf8_t& reg_a, const bhalf8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_bf16_w32_gfx12(
                 reg_a, reg_b, reg_c.template AsType<float8_t>()[Number<0>{}]);
@@ -324,7 +325,7 @@ struct intrin_wmma_i32_16x16x16_iu8_w32_gfx12<16, 16, neg_a, neg_b, clamp>
     template <class FloatC>
     __device__ static void Run(const int8x8_t& reg_a, const int8x8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<int32x8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_i32_16x16x16_iu8_w32_gfx12(
                 neg_a,
@@ -351,7 +352,7 @@ struct intrin_wmma_f32_16x16x16_f8f8_w32_gfx12<16, 16>
     template <class FloatC>
     __device__ static void Run(const f8x8_t& reg_a, const f8x8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_fp8_fp8_w32_gfx12(
                 bit_cast<int32x2_t>(reg_a),
@@ -375,7 +376,7 @@ struct intrin_wmma_f32_16x16x16_f8bf8_w32_gfx12<16, 16>
     template <class FloatC>
     __device__ static void Run(const f8x8_t& reg_a, const bf8x8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_fp8_bf8_w32_gfx12(
                 bit_cast<int32x2_t>(reg_a),
@@ -399,7 +400,7 @@ struct intrin_wmma_f32_16x16x16_bf8f8_w32_gfx12<16, 16>
     template <class FloatC>
     __device__ static void Run(const bf8x8_t& reg_a, const f8x8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_bf8_fp8_w32_gfx12(
                 bit_cast<int32x2_t>(reg_a),
@@ -423,7 +424,7 @@ struct intrin_wmma_f32_16x16x16_bf8bf8_w32_gfx12<16, 16>
     template <class FloatC>
     __device__ static void Run(const bf8x8_t& reg_a, const bf8x8_t& reg_b, FloatC& reg_c)
     {
-#if defined(__gfx120__)
+#if defined(__gfx12__)
         reg_c.template AsType<float8_t>()(Number<0>{}) =
             __builtin_amdgcn_wmma_f32_16x16x16_bf8_bf8_w32_gfx12(
                 bit_cast<int32x2_t>(reg_a),
