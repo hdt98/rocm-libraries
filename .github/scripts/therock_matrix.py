67c941fa2983e7f552909e297d1ffd66ef2d06f5
Geo Min <geomin12@amd.com>
2025-09-22 12:04:51 -0700

    [TheRock CI] Adding BLAS coverage (#1462)
    - Adding blas coverage (hipblaslt tests and rocblas tests) for
    rocm-libraries from TheRock
    - Also adding some cleanup with patching / scripts
    
    TheRock CI flow and working here:
    https://github.com/ROCm/rocm-libraries/actions/runs/17918277641/job/50946253504?pr=1462
    
    Based on changes of projects (or `.github/workflows/therock*` files),
    TheRock CI will build job(s) based on their group from
    [therock_matrix.py](https://github.com/ROCm/rocm-libraries/blob/develop/.github/scripts/therock_matrix.py)
    
    Times:
    - prim build and test: ~ 30 mins
    - rand build and test: ~ 20 mins
    - blas build and test: ~ 1h 30 mins
    
    After this PR is landed, we will be adding SPARSE to this matrix

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -2,11 +2,15 @@
 This dictionary is used to map specific file directory changes to the corresponding build flag and tests
 """
 subtree_to_project_map = {
-    "projects/rocprim": "prim",
-    "projects/rocthrust": "prim",
+    "projects/hipblas": "blas",
+    "projects/hipblas-common": "blas",
+    "projects/hipblaslt": "blas",
     "projects/hipcub": "prim",
+    "projects/hiprand": "rand",
+    "projects/rocblas": "blas",
+    "projects/rocprim": "prim",
     "projects/rocrand": "rand",
-    "projects/hiprand": "rand"
+    "projects/rocthrust": "prim"
 }
 
 project_map = {
@@ -20,4 +24,9 @@ project_map = {
         "project_to_test": "rocrand, hiprand",
         "subtree_checkout": "projects/rocrand\nprojects/hiprand",
     },
+    "blas": {
+        "cmake_options": "-DTHEROCK_ENABLE_BLAS=ON -DTHEROCK_ENABLE_ALL=OFF",
+        "project_to_test": "hipblaslt, rocblas",
+        "subtree_checkout": "projects/hipblaslt\nprojects/hipblas-common\nprojects/rocblas\nprojects/hipblas\nshared/mxdatagenerator\nshared/rocroller\nshared/tensile",
+    }
 }
