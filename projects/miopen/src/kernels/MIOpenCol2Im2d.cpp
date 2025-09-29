bc543003f5057e7b681a9a4009abd3179b3047f2
sbalint98 <balint@streamhpc.com>
2025-09-29 08:22:41 +0000

    col2im: apply clang-format

--- projects/miopen/src/kernels/MIOpenCol2Im2d.cpp
+++ projects/miopen/src/kernels/MIOpenCol2Im2d.cpp
@@ -43,24 +43,24 @@ typedef ulong index_t;
 typedef unsigned int index_t;
 #endif
 
-extern "C" __global__ void Col2Im2dU( FLOAT* col,
-                        const unsigned int col_h,
-                        const unsigned int col_w,
-                        const unsigned int wei_h,
-                        const unsigned int wei_w,
-                        const unsigned int pad_h,
-                        const unsigned int pad_w,
-                        const unsigned int stride_h,
-                        const unsigned int stride_w,
-                        const unsigned int dilation_h,
-                        const unsigned int dilation_w,
-                        const unsigned int height,
-                        const unsigned int width,
-                         FLOAT* im,
-                        const unsigned int im_offset)
+extern "C" __global__ void Col2Im2dU(FLOAT* col,
+                                     const unsigned int col_h,
+                                     const unsigned int col_w,
+                                     const unsigned int wei_h,
+                                     const unsigned int wei_w,
+                                     const unsigned int pad_h,
+                                     const unsigned int pad_w,
+                                     const unsigned int stride_h,
+                                     const unsigned int stride_w,
+                                     const unsigned int dilation_h,
+                                     const unsigned int dilation_w,
+                                     const unsigned int height,
+                                     const unsigned int width,
+                                     FLOAT* im,
+                                     const unsigned int im_offset)
 {
-    FLOAT* im_off = im + im_offset;
-    unsigned int gid              = blockIdx.x*blockDim.x + threadIdx.x;
+    FLOAT* im_off    = im + im_offset;
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
 
     unsigned int im_ch  = gid / (width * height);
     unsigned int im_pix = gid % (width * height);
@@ -68,12 +68,12 @@ extern "C" __global__ void Col2Im2dU( FLOAT* col,
     unsigned int im_w   = (im_pix % width) + pad_w;
 
     unsigned int start_h = (im_h < dilation_h * (wei_h - 1) + 1)
-                       ? 0
-                       : (im_h - (dilation_h * (wei_h - 1) + 1)) / stride_h + 1;
+                               ? 0
+                               : (im_h - (dilation_h * (wei_h - 1) + 1)) / stride_h + 1;
     unsigned int end_h   = min(col_h, im_h / stride_h + 1);
     unsigned int start_w = (im_w < dilation_w * (wei_w - 1) + 1)
-                       ? 0
-                       : (im_w - (dilation_w * (wei_w - 1) + 1)) / stride_w + 1;
+                               ? 0
+                               : (im_w - (dilation_w * (wei_w - 1) + 1)) / stride_w + 1;
     unsigned int end_w   = min(col_w, im_w / stride_w + 1);
 
     index_t ch_offset = (index_t)(im_ch * col_w * col_h) * (index_t)(wei_w * wei_h);
