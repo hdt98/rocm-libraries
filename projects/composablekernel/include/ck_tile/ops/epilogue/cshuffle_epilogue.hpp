cb36781f96da510e548039dba3cf32501b0d3ef5
Sami Aario <samaario@amd.com>
2025-10-09 08:07:04 +0000

    Introduce DetermineWarpPrecType for determining warp GEMM precision types

--- projects/composablekernel/include/ck_tile/ops/epilogue/cshuffle_epilogue.hpp
+++ projects/composablekernel/include/ck_tile/ops/epilogue/cshuffle_epilogue.hpp
@@ -7,6 +7,7 @@
 #include "ck_tile/core.hpp"
 #include "ck_tile/ops/common/utils.hpp"
 #include "ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp"
+#include "ck_tile/ops/common/determine_warp_prec_type.hpp"
 #include "ck_tile/ops/common/tensor_layout.hpp"
 #include "ck_tile/ops/elementwise/unary_element_wise_operation.hpp"
 
@@ -100,21 +101,10 @@ struct CShuffleEpilogue
     // For warp gemm selection: use tf32_t if compute type was tf32_t
     // For pk_int4/pk_fp4: use the other data type
     using ATypeToUse =
-        std::conditional_t<std::is_same_v<ADataTypeCompute, tf32_t>,
-                           tf32_t,
-                           std::conditional_t<std::is_same_v<ADataTypeBuf, pk_int4_t> ||
-                                                  std::is_same_v<ADataTypeBuf, pk_fp4_t>,
-                                              BDataTypeBuf,
-                                              ADataTypeBuf>>;
+        typename DetermineWarpPrecType<ADataTypeBuf, BDataTypeBuf, ADataTypeCompute>::a_prec_type;
     // Used for weight-only quantization kernel, B would be dequantized to the same data type as A
     using BTypeToUse =
-        std::conditional_t<std::is_same_v<BDataTypeCompute, tf32_t>,
-                           tf32_t,
-                           std::conditional_t<std::is_same_v<BDataTypeBuf, pk_int4_t> ||
-                                                  std::is_same_v<BDataTypeBuf, pk_fp4_t> ||
-                                                  sizeof(BDataTypeBuf) < sizeof(ADataTypeBuf),
-                                              ADataTypeBuf,
-                                              BDataTypeBuf>>;
+        typename DetermineWarpPrecType<ADataTypeBuf, BDataTypeBuf, BDataTypeCompute>::b_prec_type;
 
     using ELayout                          = remove_cvref_t<typename Problem::ELayout>;
     using CDElementwise                    = remove_cvref_t<typename Problem::CDElementwise>;
