40a77eea9a225fe6f74f0f987bc11cbc33d165ff
edlin102_amdeng <Ed.Lin@amd.com>
2025-10-17 11:06:03 +0800

    [Workaround] Bypass metadata layout when build hipSPARSELt.

--- projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/tensile_host.cpp
+++ projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/tensile_host.cpp
@@ -343,7 +343,8 @@ namespace
         // Add problem predicates for CEqualsD
         tensileProblem.setCEqualsD(prob.C == prob.D);
 
-        tensileProblem.setSparse(prob.sparseA ? 1 : 2);
+        // Workaround: metadata layout
+        tensileProblem.setSparse(prob.sparseA ? 1 : 2, 0);
 
         // set Actvation
         tensileProblem.setActivationType(TensileLite::ActivationType::All);
