eb4366d4e7c07a9c6548f128431d56fbefa49625
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-10-24 12:56:39 -0600

    build: always link lapack unless with the rock

--- projects/hipsparselt/cmake/hipSPARSELtLinkBLASLibraries.cmake
+++ projects/hipsparselt/cmake/hipSPARSELtLinkBLASLibraries.cmake
@@ -1,6 +1,18 @@
 # Copyright Advanced Micro Devices, Inc., or its affiliates.
 # SPDX-License-Identifier: MIT
 
+# .rst: Links the appropriate BLAS and LAPACK libraries to a target based on the build
+# configuration.
+#
+# ``hipsparselt_link_blas_libraries(<target>)``
+#
+# Links BLAS/LAPACK libraries to <target> based on the following options:
+#
+# * If HIPSPARSELT_ENABLE_THEROCK is ON: Links with TheRock's cblas package
+# * If HIPSPARSELT_ENABLE_BLIS is ON and BLIS is found: Links with BLIS and LAPACK
+# * Otherwise: Links with standard LAPACK libraries (includes BLAS)
+#
+# Assumes find_package() calls have been made prior to invocation.
 function(hipsparselt_link_blas_libraries target_name)
     if(HIPSPARSELT_ENABLE_THEROCK)
         # TheRock's cblas package provides OpenBLAS which includes BLAS, CBLAS, and LAPACK
@@ -10,7 +22,6 @@ function(hipsparselt_link_blas_libraries target_name)
         target_link_libraries(${target_name} PRIVATE BLIS::blis ${LAPACK_LIBRARIES})
         message(STATUS "Linking ${target_name} with BLIS and LAPACK")
     else()
-        # LAPACK_LIBRARIES implicitly includes BLAS (find_package(LAPACK) calls find_package(BLAS))
         target_link_libraries(${target_name} PRIVATE ${LAPACK_LIBRARIES})
         message(STATUS "Linking ${target_name} with LAPACK (includes BLAS)")
     endif()
