b7bbed6e2d2914368f609f83c52c9d668892a3ef
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2025-11-17 20:35:58 +0000

    Add division test using enableDivideBy

--- shared/rocroller/test/catch/ConvertPropagationTest.cpp
+++ shared/rocroller/test/catch/ConvertPropagationTest.cpp
@@ -43,6 +43,11 @@
 #include <rocRoller/Expression.hpp>
 #include <rocRoller/ExpressionTransformations.hpp>
 
+#include <rocRoller/KernelGraph/CoordinateGraph/Transformer.hpp>
+#include <rocRoller/KernelGraph/KernelGraph.hpp>
+#include <rocRoller/KernelGraph/RegisterTagManager.hpp>
+#include <rocRoller/Operations/Command.hpp>
+
 #include <catch2/catch_test_macros.hpp>
 
 #include <catch2/matchers/catch_matchers.hpp>
@@ -319,6 +324,153 @@ namespace ExpressionTest
         // Should not do sign extension and should do 32-bit addition
         CHECK_THAT(assembly, Catch::Matchers::ContainsSubstring("v_ashrrev_i32"));
         CHECK_THAT(assembly, Catch::Matchers::ContainsSubstring("v_add_i32"));
+    }
+
+    class DivisionKernel : public AssemblyTestKernel
+    {
+        using GD = rocRoller::Graph::Direction;
+
+    public:
+        DivisionKernel(
+            ContextPtr context, DataType resultType, DataType aType, DataType bType, DataType cType)
+            : AssemblyTestKernel(context)
+            , m_resultType(resultType)
+            , m_aType(aType)
+            , m_bType(bType)
+            , m_cType(cType)
+        {
+            makeGraph();
+        }
+
+        void generate() override
+        {
+            using namespace rocRoller;
+
+            auto kernel = m_context->kernel();
+
+            m_context->schedule(kernel->preamble());
+            m_context->schedule(kernel->prolog());
+
+            auto kb = [&]() -> Generator<Instruction> {
+                Register::ValuePtr s_result, s_a, s_b, s_c;
+                co_yield m_context->argLoader()->getValue("result", s_result);
+                co_yield m_context->argLoader()->getValue("a", s_a);
+                co_yield m_context->argLoader()->getValue("b", s_b);
+                co_yield m_context->argLoader()->getValue("c", s_c);
+
+                auto v_result
+                    = Register::Value::Placeholder(m_context,
+                                                   Register::Type::Vector,
+                                                   {m_resultType, PointerType::PointerGlobal},
+                                                   1);
+
+                auto v_a = s_a->placeholder(Register::Type::Vector, {});
+                auto v_c = s_c->placeholder(Register::Type::Vector, {});
+
+                auto a    = v_a->expression();
+                auto c    = v_c->expression();
+                auto expr = convert(rocRoller::DataType::Int32, (a / m_bExpr) + c);
+
+                Log::info("Original expr = {}", toString(expr));
+
+                co_yield v_a->allocate();
+                co_yield v_c->allocate();
+                co_yield v_result->allocate();
+
+                co_yield m_context->copier()->copy(v_result, s_result, "Move pointer");
+                co_yield m_context->copier()->copy(v_a, s_a, "Move pointer");
+                co_yield m_context->copier()->copy(v_c, s_c, "Move pointer");
+
+                Register::ValuePtr v_d;
+                // Generate the target expression
+                co_yield Expression::generate(v_d, expr, m_context);
+
+                co_yield m_context->mem()->storeGlobal(
+                    v_result, v_d, 0, DataTypeInfo::Get(m_resultType).elementBytes);
+            };
+
+            m_context->schedule(kb());
+            m_context->schedule(kernel->postamble());
+            m_context->schedule(kernel->amdgpu_metadata());
+        }
+
+    private:
+        void makeGraph()
+        {
+            using namespace rocRoller::Expression;
+            using namespace rocRoller::KernelGraph;
+            using namespace rocRoller::KernelGraph::CoordinateGraph;
+
+            auto kernel = m_context->kernel();
+
+            m_command = std::make_shared<rocRoller::Command>();
+
+            kernel->addArgument(
+                {"result", {m_resultType, PointerType::PointerGlobal}, DataDirection::WriteOnly});
+
+            auto opA  = m_command->addOperation(rocRoller::Operations::Scalar(m_aType));
+            auto argA = m_command->allocateArgument(
+                m_aType, opA, rocRoller::ArgumentType::Value, rocRoller::DataDirection::ReadOnly);
+            kernel->addArgument(
+                {"a", m_aType, rocRoller::DataDirection::ReadOnly, argA->expression()});
+
+            auto opB  = m_command->addOperation(rocRoller::Operations::Scalar(m_bType));
+            auto argB = m_command->allocateArgument(
+                m_bType, opB, rocRoller::ArgumentType::Value, rocRoller::DataDirection::ReadOnly);
+            m_bExpr = kernel->addArgument(
+                {"b", {m_bType}, rocRoller::DataDirection::ReadOnly, argB->expression()});
+
+            enableDivideBy(m_bExpr, m_context);
+
+            auto opC  = m_command->addOperation(rocRoller::Operations::Scalar(m_cType));
+            auto argC = m_command->allocateArgument(
+                m_cType, opC, rocRoller::ArgumentType::Value, rocRoller::DataDirection::ReadOnly);
+            kernel->addArgument(
+                {"c", m_cType, rocRoller::DataDirection::ReadOnly, argC->expression()});
+        }
+
+    protected:
+        DataType m_resultType, m_aType, m_bType, m_cType;
+
+        rocRoller::Expression::ExpressionPtr m_bExpr;
+
+        rocRoller::CommandPtr m_command;
+    };
+
+    TEST_CASE("Test ConvertPropagation with enableDivideBy", "[q456][gpu][convert-propagation]")
+    {
+        auto context = TestContext::ForTestDevice({{.enableFullDivision = true}});
+
+        DivisionKernel kernel(
+            context.get(), DataType::Int32, DataType::Int64, DataType::Int64, DataType::Int64);
+
+        auto d_result = make_shared_device<int32_t>();
+
+        int64_t a = (int64_t(std::numeric_limits<int32_t>::max()) << 2) + int64_t(0xffff);
+        int64_t b = 10;
+        int64_t c = 0;
+
+        std::cout << a << std::endl;
+
+        int32_t r = int32_t(int64_t(a / b) + c);
+
+        kernel({},
+               d_result.get(),
+               a,
+               b,
+               evaluate(magicMultiple(rocRoller::Expression::literal(b))),
+               evaluate(magicShiftAndSign(rocRoller::Expression::literal(b))),
+               c);
+
+        int32_t result;
+
+        hipMemcpy(&result, d_result.get(), sizeof(int32_t), hipMemcpyDefault);
+
+        std::cout << r << " / " << result << "\n";
+
+        auto const assembly = NormalizedSource(context.output());
+
+        CHECK_THAT(d_result, HasDeviceScalarEqualTo(r));
     }
 
 }
