c3035e933026af25052ccaf5681d5bc966e8b63a
Mihnea Chirila <Mihnea.Chirila@amd.com>
2025-11-18 16:50:51 -0600

    Modified tensilelite client to test kernel with custom alpha/beta params.

--- projects/hipblaslt/tensilelite/client/src/DataInitialization.cpp
+++ projects/hipblaslt/tensilelite/client/src/DataInitialization.cpp
@@ -1790,6 +1790,12 @@ namespace TensileLite
                     std::cout << "Constant " << m_cdata[i].name << ". Type "
                               << DataTypeInfo::Get(prop.dataType).abbrev << std::endl;
             }
+            //mchirila: setting custom values for alpha/beta:
+            bool custom = false;
+            if(custom){
+            m_cdata[ContractionProblemGemm::CONST::ALPHA].value =  std::complex<double>(-1.0, 3.0);
+            m_cdata[ContractionProblemGemm::CONST::BETA].value =  std::complex<double>(-3.0, 1.0);
+            }
             return;
         }
 
