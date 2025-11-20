591f17a43671b184b37d7acd757944b2893d8c38
Mihnea Chirila <mchirila@amd.com>
2025-11-20 16:32:05 +0000

    Tested, compiled and added lib logic for both C & Z datatypes on gfx942

--- shared/tensile/Tensile/Tensile.py
+++ shared/tensile/Tensile/Tensile.py
@@ -218,8 +218,8 @@ def argUpdatedGlobalParameters(args):
 def Tensile(userArgs):
     global globalParameters
     # region dbpy_attach
-    import debugpy
-    (debugpy.listen(("0.0.0.0", 5678)), debugpy.wait_for_client()) if not debugpy.is_client_connected() else None
+    # import debugpy
+    # (debugpy.listen(("0.0.0.0", 5678)), debugpy.wait_for_client()) if not debugpy.is_client_connected() else None
     # endregion
 
 
