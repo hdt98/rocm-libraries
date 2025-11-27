e424b194a347cb01b24dd5978ec6349ffd06f533
Matthew Emmett <matthew.emmett@amd.com>
2025-11-27 12:22:59 -0700

    [rocRoller] Allow UnrollLoops to do K-unrolling for StreamK kernels (#2189)
    ## Motivation
    
    Support unrolling the accumulation loop in StreamK kernels. 
    
    ## Technical Details
    
    In StreamK kernels, there are two nested loops: an outer "streaming"
    loop, and the (usual) inner "accumulation loop". The accumulation loop
    is the "K loop" and we to be able to unroll it (allowing prefetching
    etc).
    
    In the coordinate transform graph, StreamK kernels have side-by-side
    ForLoop coordinates: one for the streaming loop, and another for the
    accumulation loop. The new unroll coordinate will be placed beside the
    accumulation loop coordinate.
    
    Unfortunately, finding and placing strides for the new unroll coordinate
    is tricky: and therefore a new edge "Identify" is added, and is used to
    essentially give hints as to where strides associated with the new
    unroll should be placed.
    
    
    Next, the loop conditions of StreamK kernels has been changed so that
    they look like "iterator < max-value". That is, the top node in the
    expression is a "LessThan". This allows the existing logic around adding
    tail loops to function as before.
    
    To facilitate this, we pre-compute the number of accumulation tiles that
    need to be processed and save them in a register. This becomes the new
    "max-value" in the accumulation loop condition.
    
    ## Test Plan
    
    The StreamK tests in GEMMTest have been parameterised. A new parameter
    for the amount of unrolling has been added.
    
    New unit tests for some of the "for loop" utilities have been added.

--- shared/rocroller/lib/include/rocRoller/KernelGraph/Utils.hpp
+++ shared/rocroller/lib/include/rocRoller/KernelGraph/Utils.hpp
@@ -257,9 +257,22 @@ namespace rocRoller
         std::optional<int> findTopOfContainingOperation(int candidate, KernelGraph const& kgraph);
 
         /**
-         * @brief Create a new coordinate representing data within the scratch space. This will return a
-         * coordinate that can be added to a coordinate graph. It also allocates the required scratch space
-         * within the context.
+         * @brief Follow Identify edges.
+         *
+         * Starting from the coordinate tag, follow outgoing Identify
+         * edges until there aren't any more.  Returns the coordinate
+         * at the end of the Identify chain.
+         *
+         * If there aren't any outgoing Identify edges, this returns
+         * the original coordinate tag.
+         */
+        int followIdentify(int coordinateTag, KernelGraph const& graph);
+
+        /**
+         * @brief Create a new coordinate representing data within the
+         * scratch space. This will return a coordinate that can be
+         * added to a coordinate graph. It also allocates the required
+         * scratch space within the context.
          *
          * @param size
          * @param varType
@@ -728,6 +741,7 @@ namespace rocRoller
          * relationship in order from the root of the graph, including `control` itself.
          */
         std::deque<int> controlStack(int control, KernelGraph const& graph);
+
         /**
          * Returns all of the nodes that contain `control` with a body
          * relationship in order from the root of the graph, including `control` itself.
