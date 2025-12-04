f2dee65360e95629f3bb29aa8376b05393b7ea59
Ali Nouri <anouri@amd.com>
2025-12-04 23:12:21 +0000

    Discussion with Eugene 2

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -2352,20 +2352,20 @@ def _get_schedule_224x128x64_16bit(kernel, useLDSTr, TLDS):
     kernel["MfmaInitCVgprs"] = True
     nglshift = nllshift = 0 # vmcnt shift for ngl and nll
     optSchedule = {
-    'SYNC': [[-1, 13, 22, 22, 27, 38, 38]],                                                  # iter0 : GR Red
+    'SYNC': [[-1, 13, 22, 22, 27, 31, 31]],                                                  # iter0 : GR Red
     'LRA0': [[0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]],                               # iter1 : GR Black
-                                                                                             #         LR Black, LR Red
-                                                                                             #         
-    'GRIncA': [[0, 0, 0, 1, 1, 1, 2, 2, 2]],                                                 # iter2 : LR Red, LR Black
+                                                                                             #                    LR1 Red
+    'GRIncA': [[0, 0, 0, 1, 1, 1, 2, 2, 2]],                                                 # iter2 :         GR Red
+                                                                                             #         LR0 Red,    LR1 Black                                       
     'LRB0': [[1, 15]],
     'GRIncB': [[3, 3, 3, 4, 4, 4, 5, 5, 5]],
-    'GRA': [[22, 22, 23, 23, 24, 24, 26, 26, 27, 27, 29, 29, 30, 30]],
+    'GRA': [[                                                 22, 22, 23, 23, 24, 24, 26, 26, 27, 27, 29, 29, 30, 30]],
     'LRSA': [[26]],
     'LRSB': [[26]],
-    'GRB': [[32, 32, 33, 33, 35, 35, 36, 36]],
+    'GRB': [[                                                                                                           32, 32, 33, 33, 35, 35, 36, 36]],
     'LWSA': [[36]],
     'LWSB': [[36]],
-    'LRA1': [[39, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]],
+    'LRA1': [[                                                                                                       32, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]],
     'LRB1': [[40, 54]],
     'LCC': [[55, 55]],
     }
@@ -2376,7 +2376,7 @@ def _get_schedule_224x128x64_16bit(kernel, useLDSTr, TLDS):
         SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment=""),
         SBarrier(comment=""),
         SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="wait for prior local read local write old=0, new=0 newLW=0 newLR=0"),
-        SWaitCnt(dscnt=-1, vlcnt=11, vscnt=-1, comment="wait for previous set of global reads"),
+        SWaitCnt(dscnt=-1, vlcnt=7, vscnt=-1, comment="wait for previous set of global reads"),
         SBarrier(comment="")
     ]
     # nglshift = nllshift = 11
