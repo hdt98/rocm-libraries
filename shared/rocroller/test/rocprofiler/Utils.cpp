7650d8642471ea6b541f7e543e6b0f740949a3dd
Kerry Wang <kerrywang369@gmail.com>
2025-12-10 19:36:36 +0000

    move to utils.hpp/cpp

--- shared/rocroller/test/rocprofiler/Utils.cpp
+++ shared/rocroller/test/rocprofiler/Utils.cpp
@@ -31,6 +31,11 @@
 #include <fmt/format.h>
 #include <sstream>
 
+#include <common/Scheduling.hpp>
+#include <rocRoller/CodeGen/MemoryInstructions.hpp>
+#include <rocRoller/Expression.hpp>
+#include <rocRoller/KernelArguments.hpp>
+
 namespace rocRoller
 {
     std::string formatLatencyComparison(const std::vector<Instruction>& filteredInstructions,
@@ -81,4 +86,73 @@ namespace rocRoller
 
         return filteredInstructions;
     }
+
+    LDSTestKernelBase::LDSTestKernelBase(ContextPtr                 context,
+                                         uint32_t                   workgroupSize,
+                                         size_t                     instrDwords,
+                                         size_t                     strideMultiplier,
+                                         const std::vector<size_t>& baseAddresses,
+                                         bool                       write)
+        : AssemblyTestKernel(context)
+        , m_workgroupSize(workgroupSize)
+        , m_instrDwords(instrDwords)
+        , m_strideMultiplier(strideMultiplier)
+        , m_baseAddresses(baseAddresses)
+        , m_write(write)
+    {
+        auto k = m_context->kernel();
+        k->setKernelDimensions(1);
+
+        const auto one  = std::make_shared<Expression::Expression>(1u);
+        const auto zero = std::make_shared<Expression::Expression>(0u);
+
+        auto workitemCount = Expression::literal(m_workgroupSize * 256);
+        k->setWorkgroupSize({m_workgroupSize, 1, 1});
+        k->setWorkitemCount({workitemCount, one, one});
+        k->setDynamicSharedMemBytes(zero);
+    }
+
+    void LDSTestKernelBase::operator()()
+    {
+        KernelInvocation    invocation{{m_workgroupSize * 256, 1, 1}, {m_workgroupSize, 1, 1}, 0};
+        AssemblyTestKernel::operator()(invocation);
+    }
+
+    const std::vector<Instruction>& LDSTestKernelBase::getInstructions() const
+    {
+        return m_instructions;
+    }
+
+    void LDSTestKernelBase::generate()
+    {
+        auto k = m_context->kernel();
+
+        m_context->schedule(k->preamble());
+        m_context->schedule(k->prolog());
+
+        auto ldsData = Register::Value::AllocateLDS(
+            m_context,
+            DataType::Raw32,
+            m_context->targetArchitecture().GetCapability(GPUCapability::MaxLdsSize) / 4);
+
+        auto ldsWithOffset
+            = Register::Value::Placeholder(m_context, Register::Type::Vector, DataType::UInt32, 1);
+        auto workitemIndex = m_context->kernel()->workitemIndex()[0];
+
+        auto kernelBody = generateKernelBody(ldsData, ldsWithOffset, workitemIndex);
+
+        m_instructions.clear();
+        for(auto inst : kernelBody)
+        {
+            if(GPUInstructionInfo::isLDS(inst.getOpCode()))
+                inst.setAddresses(m_baseAddresses);
+            m_context->schedule(inst);
+            m_instructions.push_back(inst);
+        }
+        m_instructions.push_back(Instruction("s_endpgm", {}, {}, {}, ""));
+
+        m_context->schedule(k->postamble());
+        m_context->schedule(k->amdgpu_metadata());
+    }
+
 } // namespace rocRoller
--- shared/rocroller/test/rocprofiler/Utils.hpp
+++ shared/rocroller/test/rocprofiler/Utils.hpp
@@ -26,10 +26,20 @@
 
 #pragma once
 
+#include "../catch/TestContext.hpp"
+#include "../catch/TestKernels.hpp"
 #include "Agent.hpp"
+#include <rocRoller/AssemblyKernel.hpp>
 #include <rocRoller/CodeGen/Instruction.hpp>
+#include <rocRoller/Expression.hpp>
+#include <rocRoller/InstructionValues/Register.hpp>
+#include <rocRoller/Utilities/Generator.hpp>
 
+#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <common/Scheduling.hpp>
 #include <string>
+#include <tuple>
 #include <vector>
 
 namespace rocRoller
@@ -54,4 +64,93 @@ namespace rocRoller
     std::vector<Instruction>
         filterAndVerifyInstructions(const std::vector<Instruction>&                  instructions,
                                     const std::vector<profiler::InstructionProfile>& latencies);
+
+    /**
+     * @brief Base class for LDS test kernels with shared functionality
+     */
+    class LDSTestKernelBase : public AssemblyTestKernel
+    {
+    public:
+        LDSTestKernelBase(ContextPtr                 context,
+                          uint32_t                   workgroupSize,
+                          size_t                     instrDwords,
+                          size_t                     strideMultiplier,
+                          const std::vector<size_t>& baseAddresses,
+                          bool                       write);
+
+        void operator()();
+
+        const std::vector<Instruction>& getInstructions() const;
+
+    protected:
+        void generate() override;
+
+        virtual Generator<Instruction>
+            generateKernelBody(std::shared_ptr<Register::Value> ldsData,
+                               std::shared_ptr<Register::Value> ldsWithOffset,
+                               std::shared_ptr<Register::Value> workitemIndex)
+            = 0;
+
+    protected:
+        uint32_t                 m_workgroupSize;
+        size_t                   m_instrDwords;
+        size_t                   m_strideMultiplier;
+        std::vector<size_t>      m_baseAddresses;
+        bool                     m_write;
+        std::vector<Instruction> m_instructions;
+    };
+
+    /**
+     * @brief Helper function for profiling and collecting median latencies
+     */
+    template <typename KernelType>
+    std::tuple<std::vector<Instruction>, std::vector<std::tuple<std::string, size_t>>>
+        runKernelAndCollectLatencies(TestContext& context, KernelType& kernel, bool testIndividual)
+    {
+        constexpr int NUM_RUNS = 5; // Should be odd, as median is used
+
+        std::vector<std::vector<rocRoller::profiler::InstructionProfile>> allLatencies;
+
+        for(int run = 0; run < NUM_RUNS; ++run)
+        {
+            const auto latencies = rocRoller::profiler::loopUntilDispatchData([&]() { kernel(); });
+            allLatencies.push_back(latencies);
+        }
+
+        const auto& instructions = kernel.getInstructions();
+
+        const auto filteredInstructions
+            = filterAndVerifyInstructions(instructions, allLatencies[0]);
+
+        const auto infoStr = formatLatencyComparison(filteredInstructions, allLatencies[0]);
+        INFO(infoStr);
+
+        size_t expectedSize = allLatencies[0].size();
+        REQUIRE(std::all_of(
+            allLatencies.begin(), allLatencies.end(), [expectedSize](const auto& latencies) {
+                return latencies.size() == expectedSize;
+            }));
+
+        std::vector<std::tuple<std::string, size_t>> medianLatencies;
+        for(size_t i = 0; i < filteredInstructions.size(); ++i)
+        {
+            std::vector<uint64_t> latenciesPerRun;
+            for(const auto& runLatencies : allLatencies)
+            {
+                latenciesPerRun.push_back(runLatencies[i].meanLatency());
+            }
+            auto       medianLatency = median_of_odd_elements(latenciesPerRun);
+            const auto instrString   = allLatencies[0][i].instruction;
+            medianLatencies.push_back(std::make_tuple(instrString, medianLatency));
+        }
+
+        if(testIndividual)
+        {
+            Log::info(context.output());
+            Log::info(infoStr);
+        }
+
+        return std::make_tuple(filteredInstructions, medianLatencies);
+    }
+
 } // namespace rocRoller
