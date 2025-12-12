2943d9638583881a4dbc87565a7f3b0015b8b833
Adam Dickin <adam.dickin@amd.com>
2025-12-12 13:54:18 -0500

    get the coverage generating correctly

--- projects/hipdnn/cmake/Tests.cmake
+++ projects/hipdnn/cmake/Tests.cmake
@@ -100,10 +100,13 @@ function(_append_test_to_check_target_internal TARGET WORKING_DIR TEST_TYPE STAT
     endif()
 
     if(HIPDNN_ENABLE_COVERAGE)
+        # Ensure coverage report directory exists
+        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/coverage_report/profraw")
+
         # For code coverage builds, we want each profraw file to have a unique name.  The %m in the
         # _FILE_FILE environment variable will auto generate a unique id.
         list(APPEND ENVIRONMENT_LIST
-             "LLVM_PROFILE_FILE=./${CMAKE_BINARY_DIR}/coverage_report/profraw/%m.profraw"
+             "LLVM_PROFILE_FILE=${CMAKE_BINARY_DIR}/coverage_report/profraw/%m.profraw"
         )
     endif()
 
