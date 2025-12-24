2d541dac317cfbc6828ca397ea722b0192bdfd16
Qun Lin <qlin@amd.com>
2025-12-24 08:30:27 +0800

    Fix tile_example_mx_flatmm build error

--- projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp
+++ projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp
@@ -150,14 +150,13 @@ template<bool TransposeC> struct Dispatcher<fp8_t, bf8_t, float, 16, 16, 16, Tra
 template<bool TransposeC> struct Dispatcher<bf8_t, fp8_t, float, 16, 16, 16, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x16_bf8_f8<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<fp8_t, bf8_t, float, 16, 16, 64, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x64_f8_bf8<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<bf8_t, fp8_t, float, 16, 16, 64, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x64_bf8_f8<TransposeC>; };
-// Mixed precision cases
-template<bool TransposeC> struct Dispatcher<fp8_t,    pk_fp4_t, float, 16, 16, 128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f8_f4<TransposeC>; };
-template<bool TransposeC> struct Dispatcher<pk_fp4_t, pk_fp4_t, float, 16, 16, 128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f4_f4<TransposeC>; };
 
 #if defined(__gfx125__)
 template<bool TransposeC> struct Dispatcher<fp8_t, fp8_t, float, 16, 16,  64, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x64_f8_f8<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<bf8_t, bf8_t, float, 16, 16,  64, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x64_bf8_bf8<TransposeC>; };
-
+// Mixed precision cases
+template<bool TransposeC> struct Dispatcher<fp8_t,    pk_fp4_t, float, 16, 16, 128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f8_f4<TransposeC>; };
+template<bool TransposeC> struct Dispatcher<pk_fp4_t, pk_fp4_t, float, 16, 16, 128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f4_f4<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<fp8_t, fp8_t, float, 16, 16,  128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f8_f8<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<bf8_t, bf8_t, float, 16, 16,  128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_bf8_bf8<TransposeC>; };
 template<bool TransposeC> struct Dispatcher<fp8_t, bf8_t, float, 16, 16,  128, TransposeC, false> { using Type = WarpGemmWmma_f32_16x16x128_f8_bf8<TransposeC>; };
