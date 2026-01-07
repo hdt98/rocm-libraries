2ac0553a508a258ccd60e9fae3863557be3aa34b
Tianyuan Wu <tianyuan.wu@amd.com>
2026-01-07 11:27:57 +0800

    [Hipblaslt][CMS] Add CMS Support for TF32 128x192x32 TN (#3551)
    ## Motivation
    
    Add CMS scheduling for TF32 192x256x32 TN.
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    
    ## Test Plan
    - Run Tensile with parameter ```UseCustomMainLoopSchedule: [0, 1]``` to
    compare CMS on and off.
    - Run hipblaslt-bench to compare the CMS kernel and the previous default
    kernel.
    
    ## Test Result
    ### Tensile
    25.5% uplift was observed when running Tensile
    
    ### hipblaslt-bench
    No gain versus the default kernel
    ```Custom_Cijk_Alik_Bljk_S_MX_B_BIAS_HA_S_SAV_NTD_SK3_UserArgs_MT256x256x32_MI16x16x1```
    
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    ---------
    
    Signed-off-by: tywuAMD <tianyuan.wu@amd.com>
    Signed-off-by: root <root@smci355-ccs-aus-m11-13.cs-aus.dcgpu>
    Co-authored-by: root <root@smci355-ccs-aus-m11-13.cs-aus.dcgpu>

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -2254,6 +2254,71 @@ def _get_schedule_128x224x64_16bit(kernel, useLDSTr, TLDS):
     return True, opt1
 
 @RegisterSchedule(
+    tile_config=TileConfig(128, 192, 32, 2, 0, True, 0, 0),
+    dtype_predicate=isTF32,
+    vector_widths=[4, 4, 4],
+    matrix_inst=[16, 16, 32, 1],
+    mfma_wave_group=[2, 2]
+)
+def _get_schedule_128x192x32_TF32(kernel, useLDSTr, TLDS):
+    kernel["MfmaInitCVgprs"] = True
+    optSchedule = dict()
+    syncCode = []
+    nglshift = nllshift = 0 # vmcnt shift for ngl and nll
+    if isNN(kernel) and not useLDSTr and TLDS==1:
+        # TODO: Add NN schedule in upcoming PR
+        return False, None
+    elif isTN(kernel) and not useLDSTr and TLDS==1:
+        kernel["UsePLRPack"] = True
+        syncTable = [
+            -1, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Begininng of a iteration. Wait for prior local read.") ,
+            5,  SWaitCnt(dscnt=3, vlcnt=-1, vscnt=-1, comment="Before PackA0. Wait for first two LRA0. Skip 2*LRA0 + 1*LRB0.") ,
+            17, SWaitCnt(dscnt=4, vlcnt=-1, vscnt=-1, comment="Before GRA and PackB0. Wait for all prior LRA0 for GRA and the first 2*LRB0 for PackB0. Skip 4*LRB0.") ,
+            17, SBarrier(comment="GRA") ,
+            32, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Before GRB. Wait for all prior LRB0.") ,
+            32, SBarrier(comment="GRB") ,
+            35, SWaitCnt(dscnt=-1, vlcnt=6, vscnt=-1, comment="Before LRB3. Wait for GRB from previous iter. Skip 4*GRA + 2*GRB") ,
+            35, SBarrier(comment="LRB") ,
+            44, SWaitCnt(dscnt=4, vlcnt=-1, vscnt=-1, comment="Before PackB3. Wait for first two LRB3. Skip 4*LRA0.") ,
+            53, SWaitCnt(dscnt=0, vlcnt=10, vscnt=-1, comment="Before LRA3. Wait for GRA from previous iter. Skip 4*GRA + 6*GRB") ,
+            53, SBarrier(comment="LRA") ,
+            63, SWaitCnt(dscnt=2, vlcnt=-1, vscnt=-1, comment="Before PackA3. Wait for first two LRA3. Skip 2*LRA3.") ,
+        ]
+        optSchedule = {
+            'SYNC'  : [syncTable[::2]],
+            'GRIncA': [[0, 0, 0, 1, 1, 1, 2, 2, 2]],
+            'GRIncB': [[3, 3, 3, 4, 4, 4, 5, 5, 5]],
+            'LRA0'  : [[0, 1, 2, 3]],
+            'LRB0'  : [[4, 6, 8, 10, 12, 14],
+                       [4, 7, 9, 11, 13, 15]],
+            'PackA0': [create_range(5,12,17, 1, 4)],
+            'PackB0': [create_range(18,18,34, 1, 4)],
+            'GRA'   : [[17,17, 18,18, 19,19, 20,20]],
+            'GRB'   : [[33, 33, 34, 34, 41, 42, 43, 44, 51, 51, 52, 52]],
+            'LRB3'  : [[36, 37, 38, 39, 40, 41]],
+            'LRA3'  : [[53, 55, 57, 59],
+                       [54, 56, 58, 60]],
+            'PackB3': [create_range(44,9,52, 1, 8)],
+            'PackA3': [create_range(63,8,71, 1, 6)],
+            'LRSA'  : [[16]],
+            'LRSB'  : [[16]],
+            'LWSA'  : [[61]],
+            'LWSB'  : [[62]],
+            'LCC'   : [[71, 71]],
+        }
+        syncCode = syncTable[1::2]
+        nglshift = nllshift = 10
+    elif isNT(kernel) and useLDSTr and TLDS==0:
+        # TODO: Add NT schedule in upcoming PR
+        return False, None
+    else:
+        return False, None
+    
+    numMfma = 72
+    opt1 = ScheduleInfo(2, numMfma, optSchedule, syncCode, nglshift, nllshift)
+    return True, opt1
+
+@RegisterSchedule(
     tile_config=TileConfig(192, 256, 32, 2, 0, True, 0, 0),
     dtype_predicate=isTF32,
     vector_widths=[4, 4, 4],
