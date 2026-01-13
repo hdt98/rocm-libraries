73b97a6f513161fc8eb614380865a1640771c401
shbae <Seung-Hee.Bae@amd.com>
2026-01-13 06:58:59 -0800

    [hipblaslt][CMS] TF32 128x160x64 TN tile (#3751)
    ## Motivation
    
    CMS for TF 128x160x64 TN tile
    
    ## Technical Details
    
    Problem size: [2048, 2560, 8192]
    
    - **Tensile**: CMS achieves **~25% speedup** w.r.t. non-CMS
    - **hipblaslt-bench**: no gains (**~1.6% regression**)
    - **hipblaslt-bench** with `K=4096`: **8.7% speedup**.
    - - main-loop efficiency: improved from 49.7% to **63.3%**.
    
    After optimizing further by
    [2f5cb96](https://github.com/ROCm/rocm-libraries/pull/3751/commits/2f5cb96751f70b829eab39692d6a5c6ba204abc7)
    
    - **Tensile**: CMS achieves **~35% speedup** w.r.t. non-CMS
    - **hipblaslt-bench**: **~4.8% speedup** w.r.t. the custom 256x256x32
    solution.
    - **hipblaslt-bench** with `K=4096`: **17.4% speedup** w.r.t. the custom
    256x256x32 solution.
    - main-loop efficiency: improved from 49.7% to **72.9%**.
    - All tests PASSED locally.
    
    After optimizing further by
    [4e6bc13](https://github.com/ROCm/rocm-libraries/pull/3751/commits/4e6bc13802efb78381a274e59e7fcd0ec8030eed)
    
    - **Tensile**: CMS achieves **~37.5% speedup** w.r.t. non-CMS
    - **hipblaslt-bench**: **~5.1% speedup** w.r.t. the custom 256x256x32
    solution.
    - **hipblaslt-bench** with `K=4096`: **17.7% speedup** w.r.t. the custom
    256x256x32 solution.
    - main-loop efficiency: improved from 49.7% to **76.8%**.
    - All tests PASSED locally.
    
    
    ## Test Result
    
    All tests PASSED via `Tensile` as well as `hipblaslt-test`
    
    ```
            - ProblemSizes:
              - Exact: [2048, 2560, 1, 8192]
              - Range: [[128], [160], [1], [64, 64, 256]]
              - Range: [[128], [160], [1], [1,1,64]]
              - Range: [[128], [160], [1], [32, 64, 256]]
    ```
    ```
    [----------] Global test environment tear-down
    [==========] 21891 tests from 12 test suites ran. (1447922 ms total)
    [  PASSED  ] 21891 tests.
    ```
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -3273,3 +3273,98 @@ def _get_schedule_128x256x32_TF32(kernel, useLDSTr, TLDS):
     opt1 = ScheduleInfo(2, numMfma, optSchedule, syncCode, nglshift, nllshift)
     opt1.disableValidation() # Disable validation as this schedule re-order pack instructions (Non-descending-order validator to be updated to allow this)
     return True, opt1
+
+
+@RegisterSchedule(
+    tile_config=TileConfig(128, 160, 64, 2, 1, True, 0, 0),
+    dtype_predicate=isTF32,
+    vector_widths=[4, 4, 4],
+    matrix_inst=[16, 16, 32, 1],
+    mfma_wave_group=[2, 2]
+)
+def _get_schedule_128x160x64_TF32(kernel, useLDSTr, TLDS):
+    kernel["MfmaInitCVgprs"] = True
+
+    n_mfma = 120
+    optSchedule = dict()
+    nglshift = nllshift = 0
+
+    syncs = SyncSchedule()
+    syncCode = []
+    gr_inc_step = 0
+
+    if isTN(kernel) and not useLDSTr and TLDS==1:
+        kernel["UseMFMAF32XEmulation"] = True
+        kernel["UsePLRPack"] = True
+
+        grinca = [0,0,1,1,2,2,3,3,4]
+        grincb = [4,5,5,6,6,7,7,8,8]
+        lrsa   = [58]
+        lrsb   = [59]
+        lwsa   = [118]
+        lwsb   = [118]
+
+        pack_a = [0,0,1,1, 8,8, 9,9,10,10,
+                  2,2,3,3, 8,8, 11,11,12,12,
+                  4,4,5,5, 8,8, 13,13,14,14,
+                  6,6,7,7, 8,8, 15,15,16,16
+                  ]
+        pack_b = [0,0,1,1, 10,10, 11,11,12,12,
+                  2,2,3,3, 10,10, 13,13,14,14,
+                  4,4,5,5, 10,10, 15,15,16,16,
+                  6,6,7,7, 10,10, 17,17,18,18,
+                  8,8,9,9, 10,10, 19,19,20,20
+                  ]
+        lra0   = [0,1,2,3,4,5,6,7]
+        syncs.add(                 12, dscnt=4, barrier=True, comment="wait for LRA0 before pack to complete + barrier for GRA")
+        pack_a0 = [                i+13 for i in pack_a]  ## last element = 13 + 16 = 29
+
+        lrb0   = [               8,9,10,11, 13,14,15,16, 18,19]
+        syncs.add(                                               24, dscnt=0, comment="wait for LRB0 before pack to complete")
+        pack_b0 = [                                                  i+30 for i in pack_b]  ## last element = 30 + 20 = 50
+
+        gra    = [                                    17,22,27,32, 42,47,52,57] # one index for two instructions
+        grb    = [                                                               67,71,75,79, 89,93,97,101, 112,116] # one index for two instructions
+        num_gr = len(gra) + len(grb)
+
+        syncs.add(                                                            59, vlcnt=8, barrier=True, comment="wait for previous set of global reads + barrier for GRB")
+
+        lra1   = [60,61,62,63,64,65,66,67]
+        syncs.add(                          72, dscnt=4, comment="wait for LRA1 before pack to complete")
+        pack_a1 = [                         i+73 for i in pack_a]  ## last element = 73 + 16 = 89
+
+        lrb1   = [                        68,69,70,71, 73,74,75,76, 78,79]
+        syncs.add(                                                            85, dscnt=0, comment="wait for LRB1 before pack to complete")
+        pack_b1 = [                                                           i+90 for i in pack_b]  ## last element = 90 + 20 = 110
+
+        optSchedule = {
+            'SYNC':   [syncs.get_indicies()],
+            'GRIncA': [grinca],
+            'GRIncB': [grincb],
+            'LRA0':   [lra0],
+            'LRB0':   [lrb0],
+            'PackA0': [pack_a0],
+            'PackB0': [pack_b0],
+            'GRA':    [duplicate_list_items(gra, 2, gr_inc_step),
+                       duplicate_list_items([x+1 for x in gra], 2, gr_inc_step)],
+            'GRB':    [duplicate_list_items(grb, 2, gr_inc_step),
+                       duplicate_list_items([x+1 for x in grb], 2, gr_inc_step)],
+            'LRSA':   [lrsa],
+            'LRSB':   [lrsb],
+            'LWSA':   [lwsa],
+            'LWSB':   [lwsb],
+            'LRA1':   [lra1],
+            'LRB1':   [lrb1],
+            'PackB1': [pack_b1],
+            'PackA1': [pack_a1],
+            'LCC':    [[n_mfma-2, n_mfma-1]],
+        }
+
+        syncCode = syncs.get_code()
+        nglshift = nllshift = num_gr
+
+        opt1 = ScheduleInfo(2, n_mfma, optSchedule, syncCode, nglshift, nllshift)
+        return True, opt1
+
+    else:
+        return False, None
