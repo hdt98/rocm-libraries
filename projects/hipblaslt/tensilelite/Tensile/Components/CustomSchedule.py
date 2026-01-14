8550e65ae983e8f6df22de0696ab3c8c2271b8e4
emezh <emezhibo@amd.com>
2026-01-14 16:46:16 -0500

    cherry-pick e2b524fabf1 (CMS 128x128x64 TN)

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -3082,6 +3082,98 @@ def _get_schedule_128x128x32_TF32(kernel, useLDSTr, TLDS):
 
 
 @RegisterSchedule(
+    tile_config=TileConfig(128, 128, 64, 2, 1, True, 0, 0),
+    dtype_predicate=isTF32,
+    vector_widths=[4, 4, 4],
+    matrix_inst=[16, 16, 32, 1],
+    mfma_wave_group=[2, 2]
+)
+def _get_schedule_128x128x64_TF32(kernel, useLDSTr, TLDS):
+    kernel["MfmaInitCVgprs"] = True
+
+    n_mfma = 96
+    optSchedule = dict()
+    nglshift = nllshift = 0
+
+    syncs = SyncSchedule()
+    syncCode = []   
+    gr_inc_step = 1
+
+    if isTN(kernel) and not useLDSTr and TLDS==1:
+        kernel["UseMFMAF32XEmulation"] = True
+        kernel["UsePLRPack"] = True
+
+        offset=[0,0,1,1, 8,8,  9, 9,10,10, 
+                2,2,3,3, 8,8, 11,11,12,12,
+                4,4,5,5, 8,8, 13,13,14,14, 
+                6,6,7,7, 8,8, 15,15,16,16]
+        
+        lra0   = [ 0,1,2,3,4,5,6,7]
+        lrb0   = [   8,9,10,11,12,13,14,15]
+        #                wait then read
+        syncs.add(       10, dscnt=6, comment="wait for the first 2 LRAs before packing")
+        syncs.add(       14, dscnt=6, comment="wait for the rest of LRAs before packing them")
+        pack_a0= [          i+11 for i in offset] # last at 27
+        # because of GR starting at 22, we need barrier at 21, will use that for sync too.
+        syncs.add(                          21, dscnt=0, comment="wait for LRBs before the packing them",
+                                            barrier=True, barrier_comment="barrier before GR")
+        pack_b0= [                                i+28 for i in offset] # last at 44
+
+        grinca = [0,0,1,1,2,2,3,3,4]
+        grincb = [5,5,6,6,7,7,8,8,9]
+        lrsa   = [45]
+        lrsb   = [45]
+        lwsa   = [72]
+        lwsb   = [72]        
+        
+        gra    = [                            22,25,29,33, 37,41,45,49] # one index for two instructions
+        grb    = [                                                    53,57,61, 64,69,75,79,84] # one index for two instructions
+        num_gr = len(gra) + len(grb)
+
+        syncs.add(                                                 48, vlcnt=7, barrier=True, comment="wait for the previous GRs")
+
+        lra1   = [                                                 48,49,50,51,52,53,54,55]
+        lrb1   = [                                                    56,57,  59,60,61,62,63,64]
+        syncs.add(                                                          58, dscnt=6, comment="wait for the first two LRAs before packing")
+        syncs.add(                                                          63, dscnt=6, comment="wait for the rest of LRAs before packing them")
+        pack_a1 = [                                                           i+59 for i in offset] # last at 75
+        syncs.add(                                                                                 76, dscnt=0, comment="wait for LRBs before the packing them")
+        pack_b1 =[                                                                                 i+77 for i in offset] # last at 93
+
+    else:
+        return False, None  
+    
+    optSchedule = {
+        'SYNC':   [syncs.get_indicies()],
+        'GRIncA': [grinca],
+        'GRIncB': [grincb],
+        'LRA0':   [lra0],
+        'LRB0':   [lrb0],
+        'PackA0': [pack_a0],
+        'PackB0': [pack_b0],
+        'GRA':    [duplicate_list_items(gra,                2, gr_inc_step),
+                   duplicate_list_items([i+1 for i in gra], 2, gr_inc_step)],
+        'GRB':    [duplicate_list_items(grb,                2, gr_inc_step),
+                   duplicate_list_items([i+1 for i in grb], 2, gr_inc_step)],
+        'LRSA':   [lrsa],
+        'LRSB':   [lrsb],
+        'LWSA':   [lwsa],
+        'LWSB':   [lwsb],
+        'LRA1':   [lra1],
+        'LRB1':   [lrb1],
+        'PackB1': [pack_b1],
+        'PackA1': [pack_a1],
+        'LCC':    [[n_mfma-2, n_mfma-1]],
+    }
+
+    syncCode = syncs.get_code()
+    nglshift = nllshift = num_gr
+
+    opt1 = ScheduleInfo(2, n_mfma, optSchedule, syncCode, nglshift, nllshift)
+    return True, opt1
+
+
+@RegisterSchedule(
     tile_config=TileConfig(128, 256, 32, 2, 0, True, 0, 0),
     dtype_predicate=isTF32,
     vector_widths=[4, 4, 4],
