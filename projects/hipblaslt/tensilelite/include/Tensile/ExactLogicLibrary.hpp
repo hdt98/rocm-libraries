970a3557f25b321b2dccde5ab9ebc36ea6909a03
Pieter Ghysels <pieter.ghysels@amd.com>
2026-01-15 19:05:39 -0600

    Small change

--- projects/hipblaslt/tensilelite/include/Tensile/ExactLogicLibrary.hpp
+++ projects/hipblaslt/tensilelite/include/Tensile/ExactLogicLibrary.hpp
@@ -232,7 +232,12 @@ namespace TensileLite
                     solutions
                         = row.second->findTopSolutions(problem, hardware, numSolutions - rv.size());
 
-                    if(dynamic_cast<Predicates::Contraction::EqualityMatching*>(row.first.value.get()))
+                    if(dynamic_cast<Predicates::Contraction::PredictionMatching*>(row.first.value.get()))
+                    {
+                        for(auto& sol : solutions)
+                            sol->tag = MySolution::MatchingTag::Prediction;
+                    }
+                    else if(dynamic_cast<Predicates::Contraction::EqualityMatching*>(row.first.value.get()))
                     {
                         for(auto& sol : solutions)
                             sol->tag = MySolution::MatchingTag::Equal;
@@ -251,11 +256,6 @@ namespace TensileLite
                     {
                         for(auto& sol : solutions)
                             sol->tag = MySolution::MatchingTag::FreeSize;
-                    }
-                    else if(dynamic_cast<Predicates::Contraction::PredictionMatching*>(row.first.value.get()))
-                    {
-                        for(auto& sol : solutions)
-                            sol->tag = MySolution::MatchingTag::Prediction;
                     }
                     // TODO- Experimental
 
