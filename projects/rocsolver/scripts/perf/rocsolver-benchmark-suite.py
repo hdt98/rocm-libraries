06d22e776cf3c9aebed01686eafafca4d5b5ecf2
John Tyler <jetyler@ualberta.ca>
2026-01-26 14:15:09 -0700

    updated copyright year

--- projects/rocsolver/scripts/perf/rocsolver-benchmark-suite.py
+++ projects/rocsolver/scripts/perf/rocsolver-benchmark-suite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # ##########################################################################
-# Copyright (C) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
