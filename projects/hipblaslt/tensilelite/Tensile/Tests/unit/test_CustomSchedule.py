da5fb8339656f8312b3fe62f37b4e58a5a22a12d
rahjain-amd <Rahul.Jain@amd.com>
2026-01-29 11:44:04 +0530

    [hipblaslt] CMS 160x128x64 NN TF32 (#3931)
    Adding CMS for 160x128x64 NN TF32 using 4x4x4 MFMA for packing.
    
    Technical Details
    ============
    Problem size: [M, N, K] = [2560, 2048, 8192]
    
    Tensile: CMS achieves ~28.5% speedup vs. non-CMS
    main-loop efficiency: improved from 42.7% to 61% (CMS vs Non CMS)
    hipblaslt-bench with K=8192: ~11% speedup vs 256x256x32 (Default)
    
    Tests
    ====
    
    - [x] Run Tensile - Pass
    - [x] Run hipblaslt bench - Pass
    - [x] Run hipblaslt test - Pass
    - [x] pytest for this CMS - Pass
    
    ``` yaml
            - ProblemSizes:
              - Exact: [2560, 2048, 1, 8192] #t
              - Range: [[160], [128], [1], [64, 64, 256]]
              - Range: [[160], [128], [1], [1, 1, 256]]
              - Range: [[160], [128], [1], [32, 64, 256]]
    ```

--- projects/hipblaslt/tensilelite/Tensile/Tests/unit/test_CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Tests/unit/test_CustomSchedule.py
@@ -831,37 +831,40 @@ class TestCustomScheduleTF32:
 
     @pytest.mark.parametrize(
         # fmt: off
-        "transA, transB, lds_tr_inst,  tr_lds", [
-        (  True,  False,       False,       1),
+        "transA, transB, lds_tr_inst,  tr_lds, mt0, mt1", [
+        (  True,  False,       False,       1, 128, 160),
+        ( False,  False,        True,       1, 160, 128),
         # fmt: on
         ])
-    def test_schedule_128x160x64(self, transA, transB, lds_tr_inst, tr_lds):
-        """Tests the 128x160x64 TF32 TN schedule."""
+    def test_schedule_128x160x64(self, transA, transB, lds_tr_inst, tr_lds, mt0, mt1):
+        """Tests the 128x160x64 and 160x128x64 TF32 schedule."""
         kernel = create_base_kernel()
         kernel["ProblemType"].update({
             "TransposeA": transA, "TransposeB": transB
         })
+
+        du = 64
+        mi = [16,16,32,1]
+        mi_wave_group = [2, 2]
+        mi_wave_tile = (mt0 // (mi[0] * mi_wave_group[0]), mt1 // (mi[1] * mi_wave_group[1]))
+
         kernel.update({
             "UseF32XEmulation": True, "UseDirect32XEmulation": True,
-            "MacroTile0": 128, "MacroTile1": 160, "DepthU": 64,
+            "MacroTile0": mt0, "MacroTile1": mt1, "DepthU": du,
             "PrefetchGlobalRead": 2, "PrefetchLocalRead": 1,
             "GlobalReadVectorWidthA": 4, "GlobalReadVectorWidthB": 4, "LocalReadVectorWidth": 4,
-            "MatrixInstruction": [16,16,32,1], "MIWaveGroup": [2,2],
-            "LDSTrInst": lds_tr_inst, "TransposeLDS": tr_lds, "MIWaveTileA": 4, "MIWaveTileB": 5,
+            "MatrixInstruction": mi, "MIWaveGroup": mi_wave_group,
+            "LDSTrInst": lds_tr_inst, "TransposeLDS": tr_lds, "MIWaveTileA": mi_wave_tile[0], "MIWaveTileB": mi_wave_tile[1],
         })
 
         has_schedule, schedule_info = hasCustomSchedule(kernel)
         assert has_schedule
         assert isinstance(schedule_info, ScheduleInfo)
         assert schedule_info.numCodePaths == 2
-        numMfma = ((kernel["MacroTile0"] // kernel["MIWaveGroup"][0] // kernel["MatrixInstruction"][0]) *
-                   (kernel["MacroTile1"] // kernel["MIWaveGroup"][1] // kernel["MatrixInstruction"][1]) *
+        numMfma = (mi_wave_tile[0] * mi_wave_tile[1] *
                     3 * # tf32 emulated with 3 bf16
                     2   # two sub-iterations due to DepthU=64
         )
-        assert schedule_info.numMfma == numMfma
-        valid, message = isValid(schedule_info, {"kernel" : kernel})
-        assert valid, message
 
     @pytest.mark.parametrize(
         # fmt: off
