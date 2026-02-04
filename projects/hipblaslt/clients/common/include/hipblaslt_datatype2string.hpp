d6ece8fbdf308239964366570f1803c0c6ce5062
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2026-02-04 07:18:03 +0000

    MX: HipBlasLt: support MXF8B32/MXE8B16 F4

--- projects/hipblaslt/clients/common/include/hipblaslt_datatype2string.hpp
+++ projects/hipblaslt/clients/common/include/hipblaslt_datatype2string.hpp
@@ -68,9 +68,11 @@ typedef enum class _hipblaslt_scaling_format
     Scalar                  = 1,
     Vector                  = 2,
     Block_32_UE8M0          = 3,
-    Block_32_UE4M3          = 4,
-    Block_16_UE8M0          = 5,
+    Block_16_UE8M0          = 4,
+    Block_32_UE4M3          = 5,
     Block_16_UE4M3          = 6,
+    Block_32_UE5M3          = 7,
+    Block_16_UE5M3          = 8,
     Block_32_UE8M0_32_8_EXT = 1001,
 } hipblaslt_scaling_format;
 
@@ -79,9 +81,11 @@ inline bool isBlockScaling(hipblaslt_scaling_format s)
     switch(s)
     {
     case hipblaslt_scaling_format::Block_32_UE8M0:
-    case hipblaslt_scaling_format::Block_32_UE4M3:
     case hipblaslt_scaling_format::Block_16_UE8M0:
+    case hipblaslt_scaling_format::Block_32_UE4M3:
     case hipblaslt_scaling_format::Block_16_UE4M3:
+    case hipblaslt_scaling_format::Block_32_UE5M3:
+    case hipblaslt_scaling_format::Block_16_UE5M3:
     case hipblaslt_scaling_format::Block_32_UE8M0_32_8_EXT:
         return true;
     default:
@@ -95,10 +99,12 @@ inline int blockSize(hipblaslt_scaling_format s)
     {
     case hipblaslt_scaling_format::Block_32_UE8M0:
     case hipblaslt_scaling_format::Block_32_UE4M3:
+    case hipblaslt_scaling_format::Block_32_UE5M3:
     case hipblaslt_scaling_format::Block_32_UE8M0_32_8_EXT:
         return 32;
     case hipblaslt_scaling_format::Block_16_UE8M0:
     case hipblaslt_scaling_format::Block_16_UE4M3:
+    case hipblaslt_scaling_format::Block_16_UE5M3:
         return 16;
     default:
         return 1;
