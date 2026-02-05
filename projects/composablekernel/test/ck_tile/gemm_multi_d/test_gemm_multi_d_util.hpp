ac733b9d741d355538eeee107f838cc3ec0c6628
TianyuanWu <Tianyuan.Wu@amd.com>
2026-02-05 03:23:16 +0000

    Correct k warp tile size
    Signed-off-by: TianyuanWu <Tianyuan.Wu@amd.com>

--- projects/composablekernel/test/ck_tile/gemm_multi_d/test_gemm_multi_d_util.hpp
+++ projects/composablekernel/test/ck_tile/gemm_multi_d/test_gemm_multi_d_util.hpp
@@ -36,6 +36,21 @@ struct MultiplyMultiply
     }
 };
 
+#if CK_TILE_USE_WMMA
+template <typename PrecType>
+constexpr ck_tile::index_t get_k_warp_tile()
+{
+#if defined(CK_USE_GFX1250)
+    constexpr bool is_8bit = std::is_same_v<PrecType, ck_tile::fp8_t> ||
+                             std::is_same_v<PrecType, ck_tile::bf8_t> ||
+                             std::is_same_v<PrecType, ck_tile::int8_t>;
+    return is_8bit ? 64 : 32;
+#else
+    return 16;
+#endif
+}
+#endif
+
 template <typename ADataType,
           typename BDataType,
           typename AccDataType,
@@ -84,7 +99,20 @@ class TestCkTileGemmMultiD : public ::testing::Test
     using UseCshuffleEpilog = std::tuple_element_t<12, Tuple>;
     using DsLayout          = ck_tile::tuple<D0Layout, D1Layout>;
     using DsDataType        = ck_tile::tuple<D0DataType, D1DataType>;
+    using ComputeType =
+        std::conditional_t<sizeof(ADataType) < sizeof(BDataType), ADataType, BDataType>;
 
+#if CK_TILE_USE_WMMA
+    struct GemmWarpConfig_Wmma
+    {
+        static constexpr ck_tile::index_t M_Tile      = 128;
+        static constexpr ck_tile::index_t N_Tile      = 128;
+        static constexpr ck_tile::index_t K_Tile      = 64;
+        static constexpr ck_tile::index_t M_Warp_Tile = 16;
+        static constexpr ck_tile::index_t N_Warp_Tile = 16;
+        static constexpr ck_tile::index_t K_Warp_Tile = get_k_warp_tile<ComputeType>();
+    };
+#else
     struct GemmWarpConfig_Mfma
     {
         static constexpr ck_tile::index_t M_Tile      = 256;
@@ -94,16 +122,7 @@ class TestCkTileGemmMultiD : public ::testing::Test
         static constexpr ck_tile::index_t N_Warp_Tile = 32;
         static constexpr ck_tile::index_t K_Warp_Tile = 16;
     };
-
-    struct GemmWarpConfig_Wmma
-    {
-        static constexpr ck_tile::index_t M_Tile      = 128;
-        static constexpr ck_tile::index_t N_Tile      = 128;
-        static constexpr ck_tile::index_t K_Tile      = 64;
-        static constexpr ck_tile::index_t M_Warp_Tile = 16;
-        static constexpr ck_tile::index_t N_Warp_Tile = 16;
-        static constexpr ck_tile::index_t K_Warp_Tile = 16;
-    };
+#endif
 
     template <typename GemmWarpConfig,
               typename ADataType,
