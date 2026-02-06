507df4f4652f39de2e61c4b33262c1b921e430e3
Rostyslav Geyyer <rosty.geyyer@amd.com>
2026-02-06 22:04:23 +0000

    Add missing header

--- projects/composablekernel/include/ck_tile/core/numeric/vector_type.hpp
+++ projects/composablekernel/include/ck_tile/core/numeric/vector_type.hpp
@@ -10,6 +10,7 @@
 #include "ck_tile/core/numeric/bfloat16.hpp"
 #include "ck_tile/core/numeric/pk_int4.hpp"
 #include "ck_tile/core/numeric/pk_fp4.hpp"
+#include "ck_tile/core/numeric/pk_fp6.hpp"
 #include "ck_tile/core/numeric/e8m0.hpp"
 #include "ck_tile/core/utility/type_traits.hpp"
 
