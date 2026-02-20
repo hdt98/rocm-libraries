38a85eaf85c0f834e08b6a906c3d758275b2aadb
Ville Pietilä <>
2026-02-20 09:12:06 -0500

    Add more Filter3x3Stride1Pad0 instances.

--- projects/composablekernel/library/src/tensor_operation_instance/gpu/grouped_conv3d_fwd/xdl/device_grouped_conv3d_fwd_xdl_ndhwgc_gkzyxc_ndhwgk_bf16_instance.cpp
+++ projects/composablekernel/library/src/tensor_operation_instance/gpu/grouped_conv3d_fwd/xdl/device_grouped_conv3d_fwd_xdl_ndhwgc_gkzyxc_ndhwgk_bf16_instance.cpp
@@ -44,6 +44,14 @@ void add_device_grouped_conv3d_fwd_xdl_ndhwgc_gkzyxc_ndhwgk_bf16_instances(
                                                                               Empty_Tuple,
                                                                               NDHWGK,
                                                                               ConvFwd1x1S1P0>{});
+                                                                    
+    add_device_operation_instances(instances,
+                                   device_grouped_conv_fwd_xdl_bf16_instances<3,
+                                                                              NDHWGC,
+                                                                              GKZYXC,
+                                                                              Empty_Tuple,
+                                                                              NDHWGK,
+                                                                              ConvFwd3x3S1D1P0>{});
 }
 
 } // namespace instance
