4ff9c4b7751b26278c92bb0a4d5d8922b869a953
Mahesh Lakshminarasimhan <Mahesh-Lak@users.noreply.github.com>
2026-02-20 11:46:40 -0700

    Update license header
    Co-authored-by: Bryant Nelson <bryant.nelson@amd.com>

--- shared/rocroller/lib/include/rocRoller/Utilities/Settings.hpp
+++ shared/rocroller/lib/include/rocRoller/Utilities/Settings.hpp
@@ -22,7 +22,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- *******************************************************************************/
+// Copyright Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
 
 #pragma once
 
