1b0f2651dd27784464992a2066dae388a5df3872
aliry95amd <ali.yazdani@amd.com>
2026-02-23 01:53:09 +0000

    Model update

--- shared/origami/include/origami/hardware.hpp
+++ shared/origami/include/origami/hardware.hpp
@@ -152,7 +152,8 @@ class hardware_t {
       case architecture_t::gfx942:
         return {8, 17, 1.21875121875121875122 * 6, 4, 4, std::make_tuple(0, 0.015, 0), 1.5};
       case architecture_t::gfx950:
-        return {8, 17, 1.21875121875121875122 * 7, 6, 4, std::make_tuple(0, 0.008, 0), 1.5};
+        // return {8, 17, 1.21875121875121875122 * 7, 6, 4, std::make_tuple(0, 0.008, 0), 1.5};
+        return {8, 17, 1.21875121875121875122 * 7, 6, 4, std::make_tuple(-0.000013, 0.007070, 0.027355), 1.5};
       case architecture_t::gfx1201:
         return {1, 5.74, 1.21875121875121875122 * 2.41, 0.464, 2, std::make_tuple(0, 0.17, 0), 1.5};
       case architecture_t::gfx1100:
