f73c084dfc6f17e00d78abb90126946c78015f20
Julio Machado Silva <jmachado@amd.com>
2026-02-25 05:50:34 +0000

    Add simple benchmark scripts for latrd and sytrd

---
+++ projects/rocsolver/bench_latrd.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+bench=$1
+func=${2:-latrd}
+k=${3:-64}
+prec=${4:-s}
+dev=${5:-0}
+
+if [[ -z "$dev" ]]; then
+    dev=0
+fi
+
+verify=""
+if [[ ! -z ${VERIFY} ]]; then
+    verify="--verify 1"
+fi
+
+## Small & medium
+
+i=128
+while [ $i -le 256 ]
+do
+    echo -n -e "$i\t"; $bench -f $func -n $i -k $k --perf 1 --iters 10 -r $prec $verify --device $dev
+    i=$((i+32))
+done
+
+i=320
+while [ $i -le 2048 ]
+do
+    echo -n -e "$i\t"; $bench -f $func -n $i -k $k --perf 1 --iters 10 -r $prec $verify --device $dev
+    i=$((i+64))
+done
+
+### Large
+
+i=2176
+while [ $i -le 4096 ]
+do
+    echo -n -e "$i\t"; $bench -f $func -n $i -k $k --perf 1 --iters 10 -r $prec $verify --device $dev
+    i=$((i+128))
+done
+
+# i=4352
+# while [ $i -le 8192 ]
+# do
+#     echo -n -e "$i\t"; $bench -f $func -n $i -k $k --perf 1 --iters 10 -r $prec $verify --device $dev
+#     i=$((i+256))
+# done
+
+# i=8704
+# while [ $i -le 12288 ]
+# do
+#     echo -n -e "$i\t"; $bench -f $func -n $i -k $k --perf 1 --iters 10 -r $prec $verify --device $dev
+#     i=$((i+512))
+# done
+
