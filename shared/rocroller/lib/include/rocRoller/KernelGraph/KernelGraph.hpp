420d36972a4643d66adcc8b2b801566a78aa5858
Kerry Wang <kerrywang369@gmail.com>
2026-03-05 18:55:21 +0000

    use unordered_map

--- shared/rocroller/lib/include/rocRoller/KernelGraph/KernelGraph.hpp
+++ shared/rocroller/lib/include/rocRoller/KernelGraph/KernelGraph.hpp
@@ -61,7 +61,7 @@ namespace rocRoller
             CoordinateGraph::CoordinateGraph coordinates;
             ControlToCoordinateMapper        mapper;
 
-            std::map<int, std::vector<size_t>> modelledAddresses;
+            std::unordered_map<int, std::vector<size_t>> modelledAddresses;
 
             /**
             * Set up the coordinate graph and transducer for existing transformers.
