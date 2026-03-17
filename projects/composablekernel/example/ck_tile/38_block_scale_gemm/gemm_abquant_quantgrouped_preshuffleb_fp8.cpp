0630786af454c8217eb2eefcbc6c391e7ddcb3bc
khushbu <khuagarw@amd.com>
2026-03-17 17:41:12 -0400

    fixing CI issue

--- projects/composablekernel/example/ck_tile/38_block_scale_gemm/gemm_abquant_quantgrouped_preshuffleb_fp8.cpp
+++ projects/composablekernel/example/ck_tile/38_block_scale_gemm/gemm_abquant_quantgrouped_preshuffleb_fp8.cpp
@@ -15,12 +15,11 @@ static auto _ = []() {
         using BQuantGroupSize = ck_tile::QuantGroupShape<ck_tile::sequence<1, 1, 128>>;
         using TypeConfig =
             decltype(GemmQuantTypeConfig<ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, float>{});
-        return run_gemm_example_prec_type<
-            GemmConfigPrefill<ck_tile::fp8_t>,
-            TypeConfig,
-            AQuantGroupSize,
-            BQuantGroupSize,
-            ck_tile::QuantType::ABQuantGrouped>(arg_parser);
+        return run_gemm_example_prec_type<GemmConfigPrefill<ck_tile::fp8_t>,
+                                          TypeConfig,
+                                          AQuantGroupSize,
+                                          BQuantGroupSize,
+                                          ck_tile::QuantType::ABQuantGrouped>(arg_parser);
     };
     lut[hash_multiple_strings({"fp8",
                                "abquant",
@@ -31,12 +30,11 @@ static auto _ = []() {
         using BQuantGroupSize = ck_tile::QuantGroupShape<ck_tile::sequence<1, 128, 128>>;
         using TypeConfig =
             decltype(GemmQuantTypeConfig<ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, float>{});
-        return run_gemm_example_prec_type<
-            GemmConfigPrefill<ck_tile::fp8_t>,
-            TypeConfig,
-            AQuantGroupSize,
-            BQuantGroupSize,
-            ck_tile::QuantType::ABQuantGrouped>(arg_parser);
+        return run_gemm_example_prec_type<GemmConfigPrefill<ck_tile::fp8_t>,
+                                          TypeConfig,
+                                          AQuantGroupSize,
+                                          BQuantGroupSize,
+                                          ck_tile::QuantType::ABQuantGrouped>(arg_parser);
     };
     return 0;
 }();
