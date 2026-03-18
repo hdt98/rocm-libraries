092c388f243c776b0837cea7ea4add8d8a3ec330
Anton Gorenko <anton@streamhpc.com>
2026-03-18 12:16:42 +0500

    Replace magic constants with more meaningful names

--- projects/composablekernel/include/ck_tile/ops/fmha/block/cast_tile_mx.hpp
+++ projects/composablekernel/include/ck_tile/ops/fmha/block/cast_tile_mx.hpp
@@ -27,12 +27,14 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
 
     if constexpr(std::is_same_v<DstDataType, pk_fp4_t>)
     {
-        static_for<0, size / 32, 1>{}([&](auto i) {
+        constexpr index_t values_per_lane = ScaleGranularity;
+
+        static_for<0, size / values_per_lane, 1>{}([&](auto i) {
             // Maximum of consecutive ScaleGranularity values
             // (1 lane, 32 per lane for fp4)
             float max_abs = 0;
-            static_for<0, 32, 1>{}([&](auto j) {
-                max_abs = max(max_abs, abs(src_thread_buffer[number<i * 32 + j>{}]));
+            static_for<0, values_per_lane, 1>{}([&](auto j) {
+                max_abs = max(max_abs, abs(src_thread_buffer[number<i * values_per_lane + j>{}]));
             });
 
             static_assert(std::is_same_v<DstScaleDataType, e8m0_t>);
@@ -46,8 +48,11 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
 
             // Convert using scales
 
-            static_for<0, 32 / 8, 1>{}([&](auto j) {
-                using vec_t = uint32_t;
+            using vec_t                      = uint32_t;
+            constexpr index_t values_per_vec = 8;
+
+            static_for<0, values_per_lane / values_per_vec, 1>{}([&](auto j) {
+                constexpr index_t src_offset = i * values_per_lane + j * values_per_vec;
                 // These builtins require the old value, and will generate a v_mov_b32
                 // vxxx [old] before cvt, which result in unwanted ISA so we prepare an
                 // uninitialized variable x purposely, and turn off the warning
@@ -56,29 +61,30 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
                 vec_t x;
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 0>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 1>{}],
+                    src_thread_buffer[number<src_offset + 0>{}],
+                    src_thread_buffer[number<src_offset + 1>{}],
                     scale,
                     0); // byte 0
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 2>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 3>{}],
+                    src_thread_buffer[number<src_offset + 2>{}],
+                    src_thread_buffer[number<src_offset + 3>{}],
                     scale,
                     1); // byte 1
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 4>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 5>{}],
+                    src_thread_buffer[number<src_offset + 4>{}],
+                    src_thread_buffer[number<src_offset + 5>{}],
                     scale,
                     2); // byte 2
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 6>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 7>{}],
+                    src_thread_buffer[number<src_offset + 6>{}],
+                    src_thread_buffer[number<src_offset + 7>{}],
                     scale,
                     3); // byte 3
-                dst_tensor.get_thread_buffer().template set_as<vec_t>(number<i * 4 + j>{}, x);
+                dst_tensor.get_thread_buffer().template set_as<vec_t>(
+                    number<i*(values_per_lane / values_per_vec) + j>{}, x);
 #pragma clang diagnostic pop
             });
 
@@ -90,12 +96,14 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
     }
     else if constexpr(std::is_same_v<DstDataType, pk_fp6x16_t>)
     {
-        static_for<0, size / 32, 1>{}([&](auto i) {
+        constexpr index_t values_per_lane = ScaleGranularity;
+
+        static_for<0, size / values_per_lane, 1>{}([&](auto i) {
             // Maximum of consecutive ScaleGranularity values
             // (1 lane, 32 per lane for fp6)
             float max_abs = 0;
-            static_for<0, 32, 1>{}([&](auto j) {
-                max_abs = max(max_abs, abs(src_thread_buffer[number<i * 32 + j>{}]));
+            static_for<0, values_per_lane, 1>{}([&](auto j) {
+                max_abs = max(max_abs, abs(src_thread_buffer[number<i * values_per_lane + j>{}]));
             });
 
             static_assert(std::is_same_v<DstScaleDataType, e8m0_t>);
@@ -109,20 +117,28 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
 
             // Convert using scales
 
+            constexpr index_t vec_components = 6;
+            using vec_t                      = ext_vector_t<uint32_t, vec_components>;
+            constexpr index_t values_per_vec = values_per_lane;
+
+            constexpr index_t src_offset = i * values_per_lane;
             // This instruction interleaves two 16-component vectors, so src_thread_buffer must be
             // deinterleaved first
-            fp32x16_t src0, src1;
-            static_for<0, 16, 1>{}([&](auto j) {
-                src0[j()] = src_thread_buffer[number<i * 32 + j * 2 + 0>{}];
-                src1[j()] = src_thread_buffer[number<i * 32 + j * 2 + 1>{}];
+            ext_vector_t<float, values_per_vec / 2> src0, src1;
+            static_for<0, values_per_vec / 2, 1>{}([&](auto j) {
+                src0[j()] = src_thread_buffer[number<src_offset + j * 2 + 0>{}];
+                src1[j()] = src_thread_buffer[number<src_offset + j * 2 + 1>{}];
             });
-            const auto x = __builtin_amdgcn_cvt_scalef32_2xpk16_fp6_f32(src0, src1, scale);
+            const vec_t x = __builtin_amdgcn_cvt_scalef32_2xpk16_fp6_f32(src0, src1, scale);
+
             // Workaround for a compiler issue (LCOMPILER-561) with v_cvt_scalef32_2xpk16_fp6_f32:
             // src registers can be incorrectly used as dst registers
             asm(";" ::"v"(x), "v"(src0), "v"(src1), "v"(scale));
-            static_for<0, 6, 1>{}([&](auto j) {
-                dst_tensor.get_thread_buffer().template set_as<uint32_t>(number<i * 6 + j>{},
-                                                                         x[j()]);
+
+            // Need to copy each component separately because sizeof(vec_t) == sizeof(uint32_t) * 8
+            static_for<0, vec_components, 1>{}([&](auto j) {
+                dst_tensor.get_thread_buffer().template set_as<uint32_t>(
+                    number<i * vec_components + j>{}, x[j()]);
             });
 
             // Save scale for the corresponding lane
@@ -133,14 +149,16 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
     }
     else
     {
+        constexpr index_t values_per_lane = ScaleGranularity / 2;
+
         const index_t lane = __lane_id();
         float scale_result = 0;
-        static_for<0, size / 16, 1>{}([&](auto i) {
+        static_for<0, size / values_per_lane, 1>{}([&](auto i) {
             // Maximum of consecutive ScaleGranularity values
             // (2 lanes, 16 per lane for fp8/bf8)
             float max_abs = 0;
-            static_for<0, 16, 1>{}([&](auto j) {
-                max_abs = max(max_abs, abs(src_thread_buffer[number<i * 16 + j>{}]));
+            static_for<0, values_per_lane, 1>{}([&](auto j) {
+                max_abs = max(max_abs, abs(src_thread_buffer[number<i * values_per_lane + j>{}]));
             });
             // 2 lanes, 16 values per lane share one scale
             max_abs = max(max_abs, warp_shuffle(max_abs, lane ^ MLane));
@@ -157,8 +175,11 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
 
             // Convert using scales
 
-            static_for<0, 16 / 4, 1>{}([&](auto j) {
-                using vec_t = ext_vector_t<short, 2>;
+            using vec_t                      = ext_vector_t<short, 2>;
+            constexpr index_t values_per_vec = 4;
+
+            static_for<0, values_per_lane / values_per_vec, 1>{}([&](auto j) {
+                constexpr index_t src_offset = i * values_per_lane + j * values_per_vec;
                 // These builtins require the old value, and will generate a v_mov_b32
                 // vxxx [old] before cvt, which result in unwanted ISA so we prepare an
                 // uninitialized variable x purposely, and turn off the warning
@@ -169,14 +190,14 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
                 {
                     x = __builtin_amdgcn_cvt_scalef32_pk_fp8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 0>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 1>{}],
+                        src_thread_buffer[number<src_offset + 0>{}],
+                        src_thread_buffer[number<src_offset + 1>{}],
                         scale,
                         false); // false -> WORD0
                     x = __builtin_amdgcn_cvt_scalef32_pk_fp8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 2>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 3>{}],
+                        src_thread_buffer[number<src_offset + 2>{}],
+                        src_thread_buffer[number<src_offset + 3>{}],
                         scale,
                         true); // true -> WORD1
                 }
@@ -184,18 +205,19 @@ cast_tile_mx(DstTensor& dst_tensor, DstScaleTensor& dst_scale_tensor, const SrcT
                 {
                     x = __builtin_amdgcn_cvt_scalef32_pk_bf8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 0>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 1>{}],
+                        src_thread_buffer[number<src_offset + 0>{}],
+                        src_thread_buffer[number<src_offset + 1>{}],
                         scale,
                         false); // false -> WORD0
                     x = __builtin_amdgcn_cvt_scalef32_pk_bf8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 2>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 3>{}],
+                        src_thread_buffer[number<src_offset + 2>{}],
+                        src_thread_buffer[number<src_offset + 3>{}],
                         scale,
                         true); // true -> WORD1
                 }
-                dst_tensor.get_thread_buffer().template set_as<vec_t>(number<i * 4 + j>{}, x);
+                dst_tensor.get_thread_buffer().template set_as<vec_t>(
+                    number<i*(values_per_lane / values_per_vec) + j>{}, x);
 #pragma clang diagnostic pop
             });
 
