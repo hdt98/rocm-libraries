8c0d3fecab5866264098f279324fa232889e865e
Sami Aario <samaario@amd.com>
2026-03-24 11:22:25 +0000

    Move print_matrix to a separate header file under include/ck_tile/host

--- projects/composablekernel/test/ck_tile/load_and_convert_tile/test_load_and_convert_tile.cpp
+++ projects/composablekernel/test/ck_tile/load_and_convert_tile/test_load_and_convert_tile.cpp
@@ -19,36 +19,6 @@ static constexpr auto matrix_type = TestMatrixType::UniformDistribution;
 
 #define PRINT_MATRICES 0
 
-// Helper to print matrix (for debugging)
-template <typename T>
-void print_matrix(const ck_tile::HostTensor<T>& mat,
-                  const std::string& name = "Matrix",
-                  const int width         = 3,
-                  const int precision     = 3)
-{
-    const auto lens = mat.get_lengths();
-    assert(len(lens) == 2);
-    const ck_tile::index_t rows  = lens[0];
-    const ck_tile::index_t cols  = lens[1];
-    const ck_tile::index_t limit = 32;
-
-    std::cout << name << " (" << rows << "×" << cols << "):\n";
-    for(ck_tile::index_t i = 0; i < std::min(rows, ck_tile::index_t(limit)); ++i)
-    {
-        for(ck_tile::index_t j = 0; j < std::min(cols, ck_tile::index_t(limit)); ++j)
-        {
-            std::cout << std::setw(width) << std::setprecision(precision)
-                      << ck_tile::type_convert<float>(mat(i, j)) << " ";
-        }
-        if(cols > limit)
-            std::cout << "...";
-        std::cout << "\n";
-    }
-    if(rows > limit)
-        std::cout << "...\n";
-    std::cout << "\n";
-}
-
 template <typename Tuple>
 class TestLoadAndConvert : public ::testing::Test
 {
