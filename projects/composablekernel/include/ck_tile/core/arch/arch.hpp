71c8cccebc5f98fbbe908a9d51c16514e9b96c75
Cong Ma <congma13@amd.com>
2026-03-31 21:28:45 -0400

    hardcode STATIC_EVAL

--- projects/composablekernel/include/ck_tile/core/arch/arch.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/arch.hpp
@@ -28,8 +28,15 @@
 
 // Mark functions as STATIC_EVAL to enable compile-time evaluation when possible, while ensuring
 // compatibility if consteval is not supported
+//
+// TODO: CI compiler incorrectly defines STATIC_EVAL as consteval despite `-DCK_CXX_STANDARD="17"`,
+// so hardcode it here for now.
+#if 0
 #if defined(__cpp_consteval) && __cpp_consteval >= 201811L
 #define STATIC_EVAL consteval
+#else
+#define STATIC_EVAL constexpr
+#endif
 #else
 #define STATIC_EVAL constexpr
 #endif
