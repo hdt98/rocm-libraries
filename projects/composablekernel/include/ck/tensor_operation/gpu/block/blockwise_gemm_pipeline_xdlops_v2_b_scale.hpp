ffd66be5ca23e76c582c56197dd7a4f210cd4074
Marton Bidlek <marton.bidlek@streamhpc.com>
2026-04-10 07:51:30 +0000

    clang

--- projects/composablekernel/include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_v2_b_scale.hpp
+++ projects/composablekernel/include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_v2_b_scale.hpp
@@ -866,26 +866,26 @@ struct BlockwiseGemmXdlops_pipeline_v2_b_scale<BlockGemmPipelineScheduler::Inter
                                 vector_type<ComputeDataTypeBuf, KPack> a_thread_vec;
                                 vector_type<ComputeDataTypeBuf, KPack> b_thread_vec;
 
-                                auto loadA =
-                                    thread_buf_to_vec_loader<decltype(a_thread_vec),
-                                                             decltype(a_thread_buf),
-                                                             decltype(a_thread_desc_),
-                                                             ComputeDataTypeBuf,
-                                                             decltype(m0),
-                                                             Number<0>,
-                                                             decltype(k0),
-                                                             index_expression::Add<Number<k_>, index_expression::Ik>>{
-                                        a_thread_vec, a_thread_buf};
-                                auto loadB =
-                                    thread_buf_to_vec_loader<decltype(b_thread_vec),
-                                                             decltype(b_thread_buf),
-                                                             decltype(b_thread_desc_),
-                                                             ComputeDataTypeBuf,
-                                                             decltype(n0),
-                                                             Number<0>,
-                                                             decltype(k0),
-                                                             index_expression::Add<Number<k_>, index_expression::Ik>>{
-                                        b_thread_vec, b_thread_buf};
+                                auto loadA = thread_buf_to_vec_loader<
+                                    decltype(a_thread_vec),
+                                    decltype(a_thread_buf),
+                                    decltype(a_thread_desc_),
+                                    ComputeDataTypeBuf,
+                                    decltype(m0),
+                                    Number<0>,
+                                    decltype(k0),
+                                    index_expression::Add<Number<k_>, index_expression::Ik>>{
+                                    a_thread_vec, a_thread_buf};
+                                auto loadB = thread_buf_to_vec_loader<
+                                    decltype(b_thread_vec),
+                                    decltype(b_thread_buf),
+                                    decltype(b_thread_desc_),
+                                    ComputeDataTypeBuf,
+                                    decltype(n0),
+                                    Number<0>,
+                                    decltype(k0),
+                                    index_expression::Add<Number<k_>, index_expression::Ik>>{
+                                    b_thread_vec, b_thread_buf};
 
                                 static_for<0, KPack, 1>{}(MakeFunctorInvoker(loadA, loadB));
 
