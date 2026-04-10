ebe7e98274c07693ab42c6d39a3b3c6efab7e701
Marton Bidlek <marton.bidlek@streamhpc.com>
2026-04-10 07:41:52 +0000

    missing namespace

--- projects/composablekernel/include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_v2_b_scale.hpp
+++ projects/composablekernel/include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_v2_b_scale.hpp
@@ -874,7 +874,7 @@ struct BlockwiseGemmXdlops_pipeline_v2_b_scale<BlockGemmPipelineScheduler::Inter
                                                              decltype(m0),
                                                              Number<0>,
                                                              decltype(k0),
-                                                             Add<Number<k_>, index_expression::Ik>>{
+                                                             index_expression::Add<Number<k_>, index_expression::Ik>>{
                                         a_thread_vec, a_thread_buf};
                                 auto loadB =
                                     thread_buf_to_vec_loader<decltype(b_thread_vec),
@@ -884,7 +884,7 @@ struct BlockwiseGemmXdlops_pipeline_v2_b_scale<BlockGemmPipelineScheduler::Inter
                                                              decltype(n0),
                                                              Number<0>,
                                                              decltype(k0),
-                                                             Add<Number<k_>, index_expression::Ik>>{
+                                                             index_expression::Add<Number<k_>, index_expression::Ik>>{
                                         b_thread_vec, b_thread_buf};
 
                                 static_for<0, KPack, 1>{}(MakeFunctorInvoker(loadA, loadB));
