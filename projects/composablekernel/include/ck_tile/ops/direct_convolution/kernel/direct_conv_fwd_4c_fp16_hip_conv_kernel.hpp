e30a49c5d9af6e995bf793a23228a0b5ed3ffe79
Ville Pietilä <>
2026-04-15 05:11:14 -0500

    Rename hip conv files.

--- projects/composablekernel/include/ck_tile/ops/direct_convolution/kernel/direct_conv_fwd_4c_fp16_hip_conv_kernel.hpp
+++ projects/composablekernel/include/ck_tile/ops/direct_convolution/kernel/direct_conv_fwd_4c_fp16_hip_conv_kernel.hpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <tuple>
 
-#include "ck_tile/ops/direct_convolution/kernel/grouped_4c_fp16_kernel.hpp"
+#include "ck_tile/ops/direct_convolution/kernel/grouped_4c_fp16_hip_conv_impl.hpp"
 #include "ck_tile/ops/grouped_convolution/utils/grouped_convolution_utils.hpp"
 #include "ck_tile/host/kernel_launch.hpp"
 
