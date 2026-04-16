61e366627191b680f10e54955ea35211bb6ba1b7
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2026-04-16 19:21:19 -0400

    Fix SoftmaxGenerator.py SolutionLibrary.py ClientProblemFactory.hpp ReferenceValidator.cpp Serialization/ContractionSolution.hpp

--- projects/hipblaslt/tensilelite/include/Tensile/Serialization/ContractionSolution.hpp
+++ projects/hipblaslt/tensilelite/include/Tensile/Serialization/ContractionSolution.hpp
@@ -211,12 +211,7 @@ namespace TensileLite
                 iot::mapOptional(io, "mxTypeB", s.mxTypeB);
                 iot::mapOptional(io, "swizzleTensorA", s.swizzleTensorA);
                 iot::mapOptional(io, "swizzleTensorB", s.swizzleTensorB);
-<<<<<<< HEAD
-                iot::mapOptional(io, "mxBlockA", s.mxBlockA);
-                iot::mapOptional(io, "mxBlockB", s.mxBlockB);
-=======
                 iot::mapOptional(io, "metadataLayout", s.metadataLayout);
->>>>>>> origin/develop
             }
 
             const static bool flow = false;
