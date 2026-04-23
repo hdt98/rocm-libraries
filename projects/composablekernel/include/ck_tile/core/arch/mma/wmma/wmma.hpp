420e02778f934e8929206b206a54e3f2ae99811d
Chris Tsiaousis <chris.tsiaousis@streamhpc.com>
2026-04-23 12:34:14 +0000

    revert the mistakenly commited include order change
    Signed-off-by: Chris Tsiaousis <chris.tsiaousis@streamhpc.com>

--- projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma.hpp
@@ -4,8 +4,8 @@
 #pragma once
 
 // Include the architecture-specific WMMA implementations and traits
-#include "wmma_traits.hpp"
 #include "wmma_gfx11.hpp"
 #include "wmma_gfx12.hpp"
 #include "wmma_selector.hpp"
+#include "wmma_traits.hpp"
 #include "wmma_transforms.hpp"
