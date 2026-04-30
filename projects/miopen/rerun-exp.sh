a8cd90c24824f738aad2c8b406a6712500fc9cc9
Daryl Hawkins <Daryl.Hawkins@amd.com>
2026-04-29 23:54:58 -0400

    Updated experiment script

--- projects/miopen/rerun-exp.sh
+++ projects/miopen/rerun-exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd build
-ninja -j$(nproc) miopen_gtest install
+ninja -j$(nproc) miopen_gtest MIOpenDriver install
 cd ../WIP
 rm perf/*; rm -rf /tmp/.cache/miopen/*; rm -rf ~/.cache/comgr/*; AMD_COMGR_SAVE_TEMPS=1 AMD_COMGR_SAVE_LLVM_TEMPS=1 AMD_COMGR_REDIRECT_LOGS=stdout AMD_COMGR_EMIT_VERBOSE_LOGS=1 MIOPEN_LOG_LEVEL=7 MIOPEN_DEBUG_SAVE_TEMP_DIR=1 ./bench_convoclbwdwrw53.sh -r 4 6
