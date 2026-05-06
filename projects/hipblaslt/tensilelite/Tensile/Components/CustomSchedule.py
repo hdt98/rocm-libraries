92dbd149dd0c985b9d2cd71bc802d9ea6b234e3f
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2026-05-06 12:26:46 -0700

    Restore commented-out NN+TLDS=1 schedule body in _get_schedule_128x256x32_TF32
    Reverts the deletion from commit 01712a7dd9 ([yx3] Drop commented-out
    NN+TLDS=1 schedule body in CustomSchedule.py). The block sits after
    the `return False, None` in the elif branch and remains commented out;
    restoring it preserves the prior draft as in-source reference.

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -5013,6 +5013,132 @@ def _get_schedule_128x256x32_TF32(kernel, useLDSTr, TLDS):
         nglshift = nllshift = 12 # vmcnt shift for ngl and nll
     elif isNN(kernel) and TLDS==1:
         return False, None
+        # kernel["UsePLRPack"] = True
+        # kernel["UseMFMAF32XEmulation"] = True
+        # kernel["UseDot2F32XEmulation"] = False
+
+        # numLrReadA = 16
+        # numLrReadB = 8
+        # # mfma Reordering
+        # mfmaReorder = [i for i in range(0, numMfma//4)] + [i for i in range(numMfma//2, 3*numMfma//4)] + [i for i in range(numMfma//4, numMfma//2)] + [i for i in range(3*numMfma//4, numMfma)]
+
+        # # LBR0
+        # lrb0 = create_range(min_val = 0, num = numLrReadB//2, step = 1, repeat = 2)
+        # grIncB = create_range(min_val = 0, num = 3, step = 1, repeat = 3)
+        # grIncA = create_range(min_val = max(grIncB)+1, num = 3, step = 1, repeat = 3)
+        # waitLRB0 = max(lrb0)
+        # # PackB0 using mfma4x4x4_16b
+        # startPACKB0 = waitLRB0 + 4
+        # packBOffset = [ 
+        #     0, 0, 1, 1, 
+        #     8, 8,
+        #     9, 9, 10, 10,
+
+        #     2, 2, 3, 3, 
+        #     8, 8,
+        #     11, 11, 12, 12,
+
+        #     4, 4, 5, 5, 
+        #     8, 8,
+        #     13, 13, 14, 14,
+
+        #     6, 6, 7, 7, 
+        #     8, 8,
+        #     15, 15, 16, 16,
+        # ]
+        # packB0 = [x + startPACKB0 for x in packBOffset]
+        # packB0Done = max(packB0)
+        # assert packB0Done < numMfma//4
+
+        # # LRA0 
+        # lra0 = create_range(min_val = waitLRB0+4, num = numLrReadA, step = 1, repeat = 1)
+        # grA = create_range(min_val = packB0Done+6, num = 8, step = 2, repeat = 1)
+        # waitLRA0 = max(lra0)
+        # startPACKA0 = waitLRA0 + 2
+        # packAOffset = [
+        #     0, 0, 1, 1,
+        #     4, 4,
+        #     5, 5, 6, 6,
+
+        #     2, 2, 3, 3,
+        #     4, 4,
+        #     7, 7, 8, 8,
+        # ]
+        # packA0 = [x + startPACKA0 for x in packAOffset]
+        # halfMFMA = numMfma//2
+        # assert max(packA0) < halfMFMA
+
+        # # LRA3
+        # startLRA3 = halfMFMA
+        # lra3 = create_range(min_val = startLRA3, num = numLrReadA, step = 1, repeat = 1)
+        # grB = create_range(min_val = startLRA3+1, num = 4, step = 2, repeat = 2)
+        # grB += create_range(min_val = max(lra3)+1, num = 4, step = 2, repeat = 2)
+        # waitLRA3 = max(lra3)
+        # startPACKA3 = waitLRA3 + 4
+
+        # # LRB3
+        # startLRB3 = (3*numMfma)//4 - 4 # Starts 4 indexes before 3/4 MFMAs to accommodate LRB3 latency
+        # lrb3 = create_range(min_val = startLRB3, num = numLrReadB//2, step = 1, repeat = 2)
+        # waitLRB3 = max(lrb3)
+        # startPACKB3 = waitLRB3 + 4
+
+        # # Grouping segment of 4x4x4_16B MFMAs together for PackB3 & PackA3 (reduce MFMA type switching cost)
+        # packB3 = [x + startPACKB3 for x in packBOffset]
+        # start_4x4x4 = packB3[4] # 5th index is start of 4x4x4_16B MFMA for PackB3
+        # packA3 = [
+        #     *create_range(min_val = startPACKA3, num = 2, step = 1, repeat = 2),
+        #     start_4x4x4, start_4x4x4,
+        #     *create_range(min_val = max(packB3)+1, num = 2, step = 1, repeat = 2),
+
+        #     *create_range(min_val = startPACKA3+2, num = 2, step = 1, repeat = 2),
+        #     start_4x4x4, start_4x4x4,
+        #     *create_range(min_val = max(packB3)+3, num = 2, step = 1, repeat = 2),
+        # ]
+
+        # syncTable = [
+        #     waitLRB0, SWaitCnt(dscnt=min(15,numLrReadA-4), vlcnt=-1, vscnt=-1, comment="Wait for 4 LRA0 to complete"),
+        #     waitLRB0+4, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Wait for LRA0 to complete"),
+        #     waitLRB0+4, SBarrier(comment=""),
+
+        #     waitLRA0, SWaitCnt(dscnt=4, vlcnt=-1, vscnt=-1, comment="Wait for 4/8 LRB0 to complete"),
+        #     waitLRA0+4, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Wait for all LRB0 to complete"),
+        #     waitLRA0+4, SBarrier(comment=""),
+
+        #     startLRA3-1, SWaitCnt(dscnt=-1, vlcnt=6, vscnt=-1, comment="Wait for previous GRA&GRB"),
+        #     startLRA3-1, SBarrier(comment=""),
+
+        #     waitLRA3, SWaitCnt(dscnt=(numLrReadB-1), vlcnt=-1, vscnt=-1, comment="Wait for 1st LRB3 to complete"),
+        #     waitLRA3+4, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Wait for all LRB3 to complete"),
+
+        #     startLRB3-1, SWaitCnt(dscnt=-1, vlcnt=6, vscnt=-1, comment="Wait for previous GRA&GRB"),
+        #     startLRB3-1, SBarrier(comment=""),
+
+        #     waitLRB3, SWaitCnt(dscnt=min(15,numLrReadA-4), vlcnt=-1, vscnt=-1, comment="Wait for 4 LRA3 to complete"),
+        #     waitLRB3+4, SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Wait for LRA3 to complete"),
+        # ]
+
+        # optSchedule = {
+        #     'SYNC': [syncTable[::2]],
+        #     'GRIncA': [grIncA],
+        #     'GRIncB': [grIncB],
+        #     'LRA0': [lra0],
+        #     'LRB0': [lrb0],
+        #     'PackA0' : [packA0],
+        #     'PackB0' : [packB0],
+        #     'GRA': [grA],
+        #     'GRB': [grB],
+        #     'LRSA': [[max(lra0)+1]],
+        #     'LRSB': [[max(lra0)+1]],
+        #     'LWSA': [[max(lrb3)+1]],
+        #     'LWSB': [[max(lrb3)+1]],
+        #     'LCC': [[numMfma-1, numMfma-1]],
+        #     'LRA3': [lra3],
+        #     'LRB3': [lrb3],
+        #     'PackA3' : [packA3],
+        #     'PackB3' : [packB3],
+        # }
+        # syncCode = syncTable[1::2]
+        # nglshift = nllshift = 12
     else:
         return False, None
 
