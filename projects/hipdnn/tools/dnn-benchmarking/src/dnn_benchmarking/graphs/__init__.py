9ad3b63095833183e1e4753cc3a733f54eec55cb
Thomas Vy <tvy@amd.com>
2026-05-12 10:49:14 -0400

    removes the duplicate graphs

--- projects/hipdnn/tools/dnn-benchmarking/src/dnn_benchmarking/graphs/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright © Advanced Micro Devices, Inc., or its affiliates.
-# SPDX-License-Identifier:  MIT
-
-from importlib.resources import files
-
-
-def sample_graphs_path() -> str:
-    """Return the path to the bundled sample graphs directory."""
-    return str(files("dnn_benchmarking.graphs"))
