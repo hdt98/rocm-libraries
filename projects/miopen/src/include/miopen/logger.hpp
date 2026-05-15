10e437851b17c0ffa8950fea20999a12cb31668b
mousdahl-amd <mitch.ousdahl@amd.com>
2026-05-15 16:37:31 -0400

    Formatting fix

--- projects/miopen/src/include/miopen/logger.hpp
+++ projects/miopen/src/include/miopen/logger.hpp
@@ -333,23 +333,23 @@ LogParam(std::ostream& os, std::string name, const std::vector<T>& vec, bool ind
 #define MIOPEN_LOG_ROCTX_DO_LOGGING(...)
 #endif
 
-#define MIOPEN_LOG_FUNCTION(...)                                                        \
-    MIOPEN_LOG_ROCTX_DEFINE_OBJECT                                                      \
-    do                                                                                  \
-    {                                                                                   \
-        if(miopen::IsLoggingFunctionCalls())                                            \
-        {                                                                               \
-            std::ostringstream miopen_log_func_ss;                                      \
-            miopen_log_func_ss << miopen::LoggingPrefix()                               \
-                               << std::source_location::current().function_name()       \
-                               << "{" << std::endl;                                     \
-            std::cerr << miopen_log_func_ss.str();                                      \
-            MIOPEN_PP_EACH_ARGS(MIOPEN_LOG_FUNCTION_EACH, __VA_ARGS__)                  \
-            std::ostringstream().swap(miopen_log_func_ss);                              \
-            miopen_log_func_ss << miopen::LoggingPrefix() << "}" << std::endl;          \
-            std::cerr << miopen_log_func_ss.str();                                      \
-        }                                                                               \
-        MIOPEN_LOG_ROCTX_DO_LOGGING(__VA_ARGS__)                                        \
+#define MIOPEN_LOG_FUNCTION(...)                                                         \
+    MIOPEN_LOG_ROCTX_DEFINE_OBJECT                                                       \
+    do                                                                                   \
+    {                                                                                    \
+        if(miopen::IsLoggingFunctionCalls())                                             \
+        {                                                                                \
+            std::ostringstream miopen_log_func_ss;                                       \
+            miopen_log_func_ss << miopen::LoggingPrefix()                                \
+                               << std::source_location::current().function_name() << "{" \
+                               << std::endl;                                             \
+            std::cerr << miopen_log_func_ss.str();                                       \
+            MIOPEN_PP_EACH_ARGS(MIOPEN_LOG_FUNCTION_EACH, __VA_ARGS__)                   \
+            std::ostringstream().swap(miopen_log_func_ss);                               \
+            miopen_log_func_ss << miopen::LoggingPrefix() << "}" << std::endl;           \
+            std::cerr << miopen_log_func_ss.str();                                       \
+        }                                                                                \
+        MIOPEN_LOG_ROCTX_DO_LOGGING(__VA_ARGS__)                                         \
     } while(false)
 #else
 #define MIOPEN_LOG_FUNCTION(...)
@@ -365,9 +365,8 @@ constexpr std::string_view LoggingParseFunction(const std::string_view func,
     return pf_tail.substr(1 + pf_tail.find_last_of(':'));
 }
 
-inline std::string_view
-LoggingGetFnName(std::string_view func_name,
-                 std::source_location loc = std::source_location::current())
+inline std::string_view LoggingGetFnName(std::string_view func_name,
+                                         std::source_location loc = std::source_location::current())
 {
     return LoggingParseFunction(func_name, loc.function_name());
 }
