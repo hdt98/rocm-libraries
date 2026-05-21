hdt98/rocm-libraries

commit 1557ae5d33703d63ff5afcfd69f6f218119d7306

Set default LRVWAB to LRVW/2 for SPMM (#7065)
## Motivation

Previously, when LRVWA/B was not explicitly specified, it defaulted to
the full value of LRVW. However, this behavior is inconsistent with the
kernel generation logic, which assumes LRVWA/B to be half of LRVW.
This change updates the default behavior so that LRVWA/B is set to LRVW
/ 2 when not provided, ensuring consistency between the configuration
defaults and kernel generation.

## Technical Details

In the original codebase, LRVWA/B values are frequently adjusted to
derive the real vector width. With this change, LRVWA/B directly
represents the real vector width, eliminating the need for any
additional adjustments in the code.
To maintain consistency, the corresponding LRVWA/B values generated in
the logic YAML will also be updated to be half of LRVW.

## Test Plan

Run tox and hipsparselt-test on MI300, MI350, and MI450 platforms.

## Test Result

<!-- Briefly summarize test outcomes. -->

## Submission Checklist

- [x] Look over the contributing guidelines at
https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

projects/hipblaslt/tensilelite/Tensile/Components/LraTileAssignment.py
@@ -170,10 +170,8 @@ class LraTileAssignmentTransposedMFMA(LraTileAssignment):
         if kernel["ProblemType"]["Sparse"]:
           if (kernel["ProblemType"]["Sparse"] == 1 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 2 and  tP["isA"]):
             isSparseDenseMatrix = True
-          if (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and  tP["isA"]):
-            inputPerThread = inputPerThread // 2
-          elif tP["isM"]:
-            inputPerThread = inputPerThread // 8
+          if tP["isM"]:
+            inputPerThread = inputPerThread // 4
         ldsPad           = kernel["LdsPad%s" % tc] if kernel["LdsBlockSizePerPad%s" % tc] == 0 else 0
 
         # parameter for get each type index
@@ -316,10 +314,8 @@ class LraTileAssignmentTransposedMFMAB8(LraTileAssignmentTransposedMFMA):
         if kernel["ProblemType"]["Sparse"]:
           if (kernel["ProblemType"]["Sparse"] == 1 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 2 and  tP["isA"]):
             isSparseDenseMatrix = True
-          if (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and  tP["isA"]):
-            inputPerThread = inputPerThread // 2
           elif tP["isM"]:
-            inputPerThread = inputPerThread // 8
+            inputPerThread = inputPerThread // 4
         ldsPad           = kernel["LdsPad%s" % tc] if kernel["LdsBlockSizePerPad%s" % tc] == 0 else 0
 
         # parameter for get each type index
@@ -539,11 +535,8 @@ class LraTileAssignmentTransposedMFMAF4(LraTileAssignmentTransposedMFMA):
         #FIXME: tail loop with transposed load b128
         inputPerThread   = kernel[f"LocalReadVectorWidth{tc}"]
 
-        if kernel["ProblemType"]["Sparse"]:
-          if (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and  tP["isA"]):
-            inputPerThread = inputPerThread // 2
-          elif tP["isM"]:
-            inputPerThread = inputPerThread // 8
+        if kernel["ProblemType"]["Sparse"] and tP["isM"]:
+            inputPerThread = inputPerThread // 4
         ldsPad           = kernel["LdsPad%s" % tc] if kernel["LdsBlockSizePerPad%s" % tc] == 0 else 0
 
         # parameter for get each type index
@@ -649,11 +642,8 @@ class LraTileAssignmentTransposedMFMAF6(LraTileAssignmentTransposedMFMA):
         #FIXME: tail loop with transposed load b128
         inputPerThread   = kernel[f"LocalReadVectorWidth{tc}"]
 
-        if kernel["ProblemType"]["Sparse"]:
-          if (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and  tP["isA"]):
-            inputPerThread = inputPerThread // 2
-          elif tP["isM"]:
-            inputPerThread = inputPerThread // 8
+        if kernel["ProblemType"]["Sparse"] and tP["isM"]:
+            inputPerThread = inputPerThread // 4
         ldsPad           = kernel["LdsPad%s" % tc] if kernel["LdsBlockSizePerPad%s" % tc] == 0 else 0
 
         # parameter for get each type index
@@ -782,11 +772,8 @@ class LraTileAssignmentMFMA(LraTileAssignment):
         # use MIInputPerThread if lrvw < MIInputPerThread
         if isgfx950 and tP["bpeDS"] == 0.5 and lrvw < kernel["MIInputPerThread%s"%tc]:
             inputPerThread = kernel["MIInputPerThread%s"%tc]
-        if kernel["ProblemType"]["Sparse"]:
-          if (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and tP["isA"]):
-            inputPerThread = inputPerThread // 2
-          elif tP["isM"]:
-            inputPerThread = inputPerThread // 8
+        if kernel["ProblemType"]["Sparse"] and tP["isM"]:
+            inputPerThread = inputPerThread // 4
         LdsPad           = kernel["LdsPad%s" % tc] if kernel["LdsBlockSizePerPad%s" % tc] == 0 else 0
 
         # parameter for get each type index

projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -12132,7 +12132,7 @@ class KernelWriterAssembly(KernelWriter):
           else:
             if tc == "A":
               sparseA = kernel["ProblemType"]["Sparse"] == 1
-              lrvw = kernel["LocalReadVectorWidth%s"%tc] // (2 if sparseA else 1)
+              lrvw = kernel["LocalReadVectorWidth%s"%tc]
               wlr = max(lrvw//kernel["MIInputPerThreadA"], 1)
               if kernel["ProblemType"]["Sparse"] and lrvw < kernel["MIInputPerThreadA"]:
                 if not sparseA:
@@ -12150,7 +12150,7 @@ class KernelWriterAssembly(KernelWriter):
             elif tc in ("MXSA", "MXSB"):
               offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (matrixInstK)
             elif tc == "Metadata":
-              lrvw = kernel["LocalReadVectorWidth%s"%tc] // 8
+              lrvw = kernel["LocalReadVectorWidth%s"%tc] // 4
               wlr = max(lrvw//kernel["MIInputPerThreadMetadata"], 1)
               if lrvw < kernel["MIInputPerThreadMetadata"]:
                 offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"]*lrvw//kernel["MIInputPerThreadMetadata"]) // 4
@@ -12163,7 +12163,7 @@ class KernelWriterAssembly(KernelWriter):
                 offsetInc //= 8
             elif tc == "B":
               sparseB = kernel["ProblemType"]["Sparse"] == 2
-              lrvw = kernel["LocalReadVectorWidth%s"%tc] // (2 if sparseB else 1)
+              lrvw = kernel["LocalReadVectorWidth%s"%tc]
               wlr = max(lrvw//kernel["MIInputPerThreadB"], 1)
               if kernel["ProblemType"]["Sparse"] and lrvw < kernel["MIInputPerThreadB"]:
                 if not sparseB:

commit 31f88d5ba739efd45f1f0f6de39bd074ca847766

[AIHPSPAR-101] Fix incorrect results with LDSTrInst=True (#7573)
## Motivation

Fix failures observed in **LDSTrInst=True** cases introduced by
https://github.com/ROCm/rocm-libraries/pull/7065.

## Technical Details

1. Refactor the undeleted sparse factor of 2.
2. Remove the use of LRVW in the LDSTrInst case, as it is not actually
used to determine the instruction.
3. Remove the constraint LRVWA = LRVW / 2 to allow sparse matrices to
load arbitrary sizes.

## Test Plan

Run tox and hipsparselt-test on the following platforms:
- MI300
- MI350
- FFM

## Test Result

<!-- Briefly summarize test outcomes. -->

## Submission Checklist

- [x] Look over the contributing guidelines at
https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

projects/hipblaslt/tensilelite/Tensile/Components/LraTileAssignment.py
@@ -841,7 +841,7 @@ class LraTileAssignmentMFMA(LraTileAssignment):
         if kernel["ProblemType"]["Sparse"] != 0:
             if kernel["MIInputPerThread"] * kernel["ProblemType"]["DataType"].numBytes() > 16:
                 isSparseTrack = (kernel["ProblemType"]["Sparse"] == 2 and tP["isB"]) or (kernel["ProblemType"]["Sparse"] == 1 and tP["isA"]) or tP["isM"]
-                strideK       = (inputPerThread if umlds else (mt + LdsPad) * inputPerThread) * (2 if isSparseTrack and kernel["MIInputPerThread%s"%tc] > inputPerThread else 1)
+                strideK       = inputPerThread if umlds else (mt + LdsPad) * inputPerThread
             # GFX1250 Sparse
             if writer.states.asmCaps["HasSWMMAC"] and writer.states.asmCaps["HasSWMMAC_gfx1250"] and (not isSparseTrack or tP["isM"]):
                 strideK *= 2

projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -11963,12 +11963,9 @@ class KernelWriterAssembly(KernelWriter):
               lrvw = kernel["LocalReadVectorWidth%s"%tc]
               wlr = max(lrvw//kernel["MIInputPerThreadA"], 1)
               if kernel["ProblemType"]["Sparse"] and lrvw < kernel["MIInputPerThreadA"]:
-                if not sparseA:
-                  offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"])
-                else:
-                  offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"]*lrvw//kernel["MIInputPerThreadA"])
-                  if kernel["WavefrontSize"] == 32:
-                    offsetInc *= 2
+                offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"])
+                if sparseA:
+                  offsetInc //= 2
               elif (self.states.localReadDoCntA)%(wlr):
                 offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * kernel["MIInputPerThreadA"]
               else:
@@ -11981,9 +11978,7 @@ class KernelWriterAssembly(KernelWriter):
               lrvw = kernel["LocalReadVectorWidth%s"%tc] // 4
               wlr = max(lrvw//kernel["MIInputPerThreadMetadata"], 1)
               if lrvw < kernel["MIInputPerThreadMetadata"]:
-                offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"]*lrvw//kernel["MIInputPerThreadMetadata"]) // 4
-                if kernel["WavefrontSize"] == 32:
-                  offsetInc *= 2
+                offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * kernel["MatrixInstK"] // 8
               elif (self.states.localReadDoCntMetadata)%(wlr):
                 offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * kernel["MIInputPerThreadMetadata"]
               else:
@@ -11994,12 +11989,9 @@ class KernelWriterAssembly(KernelWriter):
               lrvw = kernel["LocalReadVectorWidth%s"%tc]
               wlr = max(lrvw//kernel["MIInputPerThreadB"], 1)
               if kernel["ProblemType"]["Sparse"] and lrvw < kernel["MIInputPerThreadB"]:
-                if not sparseB:
-                  offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"])
-                else:
-                  offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"]*lrvw//kernel["MIInputPerThreadB"])
-                  if kernel["WavefrontSize"] == 32:
-                    offsetInc *= 2
+                offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * (kernel["MatrixInstK"])
+                if sparseB:
+                  offsetInc //= 2
               elif (self.states.localReadDoCntB)%(wlr):
                 offsetInc = (kernel["MacroTile%s"%tP["tensorChar"]] + LdsPad) * kernel["MIInputPerThreadB"]
               else:

projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
@@ -2666,12 +2666,12 @@ class Solution(collections.abc.Mapping):
 
       if state["LocalReadVectorWidthA"] == -1:
         state["LocalReadVectorWidthA"] = state["LocalReadVectorWidth"]
-        if (not state["enableLDSTrA"]) and (state["ProblemType"]["Sparse"] == 1) and (state["LocalReadVectorWidthA"] != -1):
-          state["LocalReadVectorWidthA"] //= 2
+        if (not state["enableLDSTrA"]) and (state["ProblemType"]["Sparse"] == 1):
+          state["LocalReadVectorWidthA"] = min(state["LocalReadVectorWidthA"], state["MIInputPerThreadA"])
       if state["LocalReadVectorWidthB"] == -1:
         state["LocalReadVectorWidthB"] = state["LocalReadVectorWidth"]
-        if (not state["enableLDSTrB"]) and (state["ProblemType"]["Sparse"] == 2) and (state["LocalReadVectorWidthB"] != -1):
-          state["LocalReadVectorWidthB"] //= 2
+        if (not state["enableLDSTrB"]) and (state["ProblemType"]["Sparse"] == 2):
+          state["LocalReadVectorWidthB"] = min(state["LocalReadVectorWidthB"], state["MIInputPerThreadB"])
 
       def calLRVW():
         # Default LocalReadVectorWidth
@@ -2679,8 +2679,6 @@ class Solution(collections.abc.Mapping):
           # Default LocalReadVectorWidth
           autoLRVWA = False
           maxNumDsLoadBytesA = Solution.MAX_NUM_DS_LOAD_BYTES
-          if (not state["enableLDSTrA"]) and (state["ProblemType"]["Sparse"] == 1):
-            maxNumDsLoadBytesA //= 2
           maxLRVWA = int(maxNumDsLoadBytesA // state["ProblemType"]["MacDataTypeA"].numBytes())
           # Set maxLRVW to 32 for 6 bits float: use two load instructions b128(4 vgpr) and b64(2 vgpr) to mimic b192
           if isaInfoMap[isa].asmCaps["HasWMMA_f8f6f4"] and state["ProblemType"]["MacDataTypeA"].numBytes() == 0.75:
@@ -2716,8 +2714,6 @@ class Solution(collections.abc.Mapping):
           # Default LocalReadVectorWidth
           autoLRVWB = False
           maxNumDsLoadBytesB = Solution.MAX_NUM_DS_LOAD_BYTES
-          if (not state["enableLDSTrB"]) and (state["ProblemType"]["Sparse"] == 2):
-            maxNumDsLoadBytesB //= 2
           maxLRVWB = int(maxNumDsLoadBytesB // state["ProblemType"]["MacDataTypeB"].numBytes())
           # Set maxLRVW to 32 for 6 bits float: use two load instructions b128(4 vgpr) and b64(2 vgpr) to mimic b192
           if isaInfoMap[isa].asmCaps["HasWMMA_f8f6f4"] and state["ProblemType"]["MacDataTypeB"].numBytes() == 0.75:
@@ -2779,12 +2775,8 @@ class Solution(collections.abc.Mapping):
             wlrB = max(state["LocalReadVectorWidthB"] // state["MIInputPerThreadB"], 1)
             if wlrA > wlrB:
               state["LocalReadVectorWidthA"] = wlrB * state["MIInputPerThreadA"]
-              if state["ProblemType"]["Sparse"] == 1:
-                state["LocalReadVectorWidthA"] //= 2
             if wlrA < wlrB:
               state["LocalReadVectorWidthB"] = wlrA * state["MIInputPerThreadB"]
-              if state["ProblemType"]["Sparse"] == 2:
-                state["LocalReadVectorWidthB"] //= 2
 
           if state["ProblemType"]["Sparse"] == 1:
             state["LocalReadVectorWidthMetadata"] = state["LocalReadVectorWidthA"]
@@ -2877,12 +2869,8 @@ class Solution(collections.abc.Mapping):
             wlrB = max(state["LocalReadVectorWidthB"] // state["MIInputPerThreadB"], 1)
             if wlrA > wlrB:
               state["LocalReadVectorWidthA"] = wlrB * state["MIInputPerThreadA"]
-              if state["ProblemType"]["Sparse"] == 1:
-                state["LocalReadVectorWidthA"] //= 2
             if wlrA < wlrB:
               state["LocalReadVectorWidthB"] = wlrA * state["MIInputPerThreadB"]
-              if state["ProblemType"]["Sparse"] == 2:
-                state["LocalReadVectorWidthB"] //= 2
 
           if state["ProblemType"]["Sparse"] == 1:
             state["LocalReadVectorWidthMetadata"] = state["LocalReadVectorWidthA"]

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Ailk_Bjlk_I8IS_BH_Bias_FDMN_SPAML0_A_S_SAV.yaml
@@ -196,7 +196,7 @@
     LdsPadMXSB: 0
     LdsPadMetadata: 4
     LocalReadVectorWidth: 16
-    LocalReadVectorWidthA: 8
+    LocalReadVectorWidthA: 16
     LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Ailk_Bjlk_I8IS_BH_Bias_FDMN_SPBML0_A_S_SAV.yaml
@@ -197,7 +197,7 @@
     LdsPadMetadata: 0
     LocalReadVectorWidth: 16
     LocalReadVectorWidthA: 16
-    LocalReadVectorWidthB: 8
+    LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1
     LocalSplitUReuseLDS: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Ailk_Bljk_I8IS_BH_Bias_FDMN_SPAML0_A_S_SAV.yaml
@@ -196,7 +196,7 @@
     LdsPadMXSB: 0
     LdsPadMetadata: 4
     LocalReadVectorWidth: 16
-    LocalReadVectorWidthA: 8
+    LocalReadVectorWidthA: 16
     LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Ailk_Bljk_I8IS_BH_Bias_FDMN_SPBML0_A_S_SAV.yaml
@@ -197,7 +197,7 @@
     LdsPadMetadata: 0
     LocalReadVectorWidth: 16
     LocalReadVectorWidthA: 16
-    LocalReadVectorWidthB: 8
+    LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1
     LocalSplitUReuseLDS: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Alik_Bjlk_I8IS_BH_Bias_FDMN_SPAML0_A_S_SAV.yaml
@@ -196,7 +196,7 @@
     LdsPadMXSB: 0
     LdsPadMetadata: 4
     LocalReadVectorWidth: 16
-    LocalReadVectorWidthA: 8
+    LocalReadVectorWidthA: 16
     LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Alik_Bjlk_I8IS_BH_Bias_FDMN_SPBML0_A_S_SAV.yaml
@@ -197,7 +197,7 @@
     LdsPadMetadata: 0
     LocalReadVectorWidth: 16
     LocalReadVectorWidthA: 16
-    LocalReadVectorWidthB: 8
+    LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1
     LocalSplitUReuseLDS: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Alik_Bljk_I8IS_BH_Bias_FDMN_SPAML0_A_S_SAV.yaml
@@ -196,7 +196,7 @@
     LdsPadMXSB: 0
     LdsPadMetadata: 4
     LocalReadVectorWidth: 16
-    LocalReadVectorWidthA: 8
+    LocalReadVectorWidthA: 16
     LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1

projects/hipsparselt/library/src/hcc_detail/rocsparselt/src/spmm/Tensile/Logic/asm_full/gfx1250/Gridbased/gfx1250_Cijk_Alik_Bljk_I8IS_BH_Bias_FDMN_SPBML0_A_S_SAV.yaml
@@ -197,7 +197,7 @@
     LdsPadMetadata: 0
     LocalReadVectorWidth: 16
     LocalReadVectorWidthA: 16
-    LocalReadVectorWidthB: 8
+    LocalReadVectorWidthB: 16
     LocalReadVectorWidthMetadata: 16
     LocalSplitU: 1
     LocalSplitUReuseLDS: 1